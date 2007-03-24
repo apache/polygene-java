0dc4f8747fda6481f185eb7107fd715cdb3b3d1a
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-03-24 14:57:12 +0000

    Trying to find good package names in the libraries.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/library@5945 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- business/src/main/java/org/qi4j/business/organization/PersonValidationPointcut.java
+++ business/src/main/java/org/qi4j/business/organization/PersonValidationPointcut.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.qi4j.business.entity.pointcut;
+package org.qi4j.business.organization;
 
 import org.qi4j.runtime.pointcut.constraints.QiImplements;
 import org.qi4j.runtime.pointcut.QiInterceptor;
