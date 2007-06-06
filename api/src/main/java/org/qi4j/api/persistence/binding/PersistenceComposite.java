33e243361984fb50a2ae4e20ae57cd995f514945
rickard.oberg@senselogic.se <rickard.oberg@senselogic.se@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-06-06 12:14:03 +0000

    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@6733 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ api/src/main/java/org/qi4j/api/persistence/binding/PersistenceComposite.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2007, Rickard Öberg. All Rights Reserved.
+ * Copyright (c) 2007, Niclas Hedhman. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.qi4j.api.persistence.composite;
+
+import org.qi4j.api.annotation.ModifiedBy;
+import org.qi4j.api.persistence.Identity;
+import org.qi4j.api.persistence.Persistent;
+import org.qi4j.api.persistence.Lifecycle;
+import org.qi4j.api.persistence.modifier.ReadUpdateModifier;
+
+/**
+ * Persistent domain objects should implement this composite
+ * which provides an identity and a reference to a repository
+ */
+@ModifiedBy( ReadUpdateModifier.class )
+public interface PersistenceComposite extends Identity, Persistent, Lifecycle
+{
+}
