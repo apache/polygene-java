08a6a5ec98dbf5a1c9831fb2e6afd35d9781110e
rickard.oberg@senselogic.se <rickard.oberg@senselogic.se@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-06-10 18:04:22 +0000

    Massive refactoring (again): * Composite, Modifier, Mixin now only represent static information * CompositeInterface contains runtime structure * InvocationInstance management simplified * II pooling refactored into ObjectContext * ObjectInvocationHandler simplified (!) * Removed some seemingly unnecessary interfaces * FragmentFactory now has more context to use for instantiation * FF instantiation split into Modifier and Mixin * SPI cleaned up (maybe too much?) * Many other minor details
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@6781 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ runtime/src/test/java/org/qi4j/model/DomainInterface.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2007 Rickard Öberg
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.qi4j.model;
+
+import org.qi4j.api.annotation.ImplementedBy;
+
+/**
+ * TODO
+ *
+ * @author rickard
+ * @version $Revision: 1.7 $
+ */
+@ImplementedBy( DomainInterfaceImpl.class )
+public interface DomainInterface
+{
+    // Public --------------------------------------------------------
+    String getFoo();
+
+    void setFoo( String foo );
+}
