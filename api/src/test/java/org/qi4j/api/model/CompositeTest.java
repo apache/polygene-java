f546ac916e50071c6610a9ba9682368afde53877
rickard.oberg@senselogic.se <rickard.oberg@senselogic.se@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-06-10 18:05:41 +0000

    Massive refactoring (again): * Composite, Modifier, Mixin now only represent static information * CompositeInterface contains runtime structure * InvocationInstance management simplified * II pooling refactored into ObjectContext * ObjectInvocationHandler simplified (!) * Removed some seemingly unnecessary interfaces * FragmentFactory now has more context to use for instantiation * FF instantiation split into Modifier and Mixin * SPI cleaned up (maybe too much?) * Many other minor details
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@6783 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ api/src/test/java/org/qi4j/api/model/CompositeTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2007 Rickard Öberg
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.qi4j.api.model;
+
+import junit.framework.TestCase;
+import org.qi4j.api.ObjectFactory;
+import org.qi4j.runtime.ObjectFactoryImpl;
+import org.qi4j.test.model.CustomTestComposite;
+import org.qi4j.test.model.DomainInterface;
+import org.qi4j.test.model.DomainInterfaceImpl;
+import org.qi4j.test.model.Standard;
+import org.qi4j.test.model.StandardThat;
+import org.qi4j.test.model.StandardThatImpl;
+import org.qi4j.test.model.StandardThis;
+import org.qi4j.test.model.StandardThisImpl;
+import org.qi4j.test.model.TestComposite;
+
+public class CompositeTest extends TestCase
+{
+    Composite composite;
+
+    protected void setUp() throws Exception
+    {
+        composite = new Composite( TestComposite.class );
+    }
+
+    public void testGetImplementation() throws Exception
+    {
+        assertEquals( 0, composite.getImplementations( Standard.class ).size() );
+
+        System.out.println( composite );
+
+        assertEquals( DomainInterfaceImpl.class, composite.getImplementations( DomainInterface.class ).get( 0 ).getFragmentClass() );
+        assertEquals( StandardThisImpl.class, composite.getImplementations( StandardThis.class ).get( 0 ).getFragmentClass() );
+        assertEquals( StandardThatImpl.class, composite.getImplementations( StandardThat.class ).get( 0 ).getFragmentClass() );
+
+        ObjectFactory factory = new ObjectFactoryImpl();
+
+        {
+            TestComposite object = factory.newInstance( TestComposite.class );
+
+            assertEquals( "bar=foo:FOO Hello World", object.foo( "FOO " ) );
+
+            object.setFoo( "xyz" );
+            try
+            {
+                object.setFoo( null );
+                fail( "Should have thrown an exception" );
+            }
+            catch( Exception e )
+            {
+                // Ok
+            }
+        }
+
+        {
+            TestComposite object = factory.newInstance( CustomTestComposite.class );
+
+            object.setFoo( "xyz" );
+            assertEquals( "FOO:foo:xyz", object.getFoo() );
+
+            System.out.println( object.getComposite() );
+
+            object.setFoo( null );
+        }
+    }
+}
