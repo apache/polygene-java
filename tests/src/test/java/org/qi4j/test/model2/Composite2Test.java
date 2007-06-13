9301b0a5e774d0f446170e740ab02c9e7dc10402
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-06-13 08:32:13 +0000

    o Moved API tests to core-tests module. o Added InvalidCompositeException to be thrown if the composite interface is not extending the Composite or if the Composite is a class. o Added tests for that. o Introduced DependencyResolver. o Broke out CompositeContext as interface, to be passed to the DependencyResolver.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@6895 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ tests/src/test/java/org/qi4j/test/model2/Composite2Test.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2007 Rickard Öberg
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.qi4j.test.model2;
+
+import junit.framework.TestCase;
+import org.qi4j.api.CompositeFactory;
+import org.qi4j.api.model.CompositeModel;
+import org.qi4j.runtime.CompositeFactoryImpl;
+
+public class Composite2Test extends TestCase
+{
+    CompositeModel composite;
+
+    protected void setUp() throws Exception
+    {
+        composite = new CompositeModel( TestComposite.class );
+    }
+
+    public void testGetImplementation() throws Exception
+    {
+        assertEquals( 0, composite.getImplementations( Standard.class ).size() );
+
+        System.out.println( composite );
+
+        assertEquals( DomainInterfaceImpl.class, composite.getImplementations( DomainInterface.class ).get( 0 ).getFragmentClass() );
+        assertEquals( StandardThisImpl.class, composite.getImplementations( StandardThis.class ).get( 0 ).getFragmentClass() );
+        assertEquals( StandardThatImpl.class, composite.getImplementations( StandardThat.class ).get( 0 ).getFragmentClass() );
+
+        CompositeFactory factory = new CompositeFactoryImpl();
+
+        {
+            TestComposite object = factory.newInstance( TestComposite.class );
+
+            assertEquals( "bar=foo:FOO Hello World", object.foo( "FOO " ) );
+
+            object.setFoo( "xyz" );
+            try
+            {
+                object.setFoo( null );
+                fail( "Should have thrown an exception" );
+            }
+            catch( Exception e )
+            {
+                // Ok
+            }
+        }
+
+        {
+            TestComposite object = factory.newInstance( CustomTestComposite.class );
+
+            object.setFoo( "xyz" );
+            assertEquals( "FOO:foo:xyz", object.getFoo() );
+
+            System.out.println( object.getCompositeObject());
+
+            object.setFoo( null );
+        }
+    }
+}
