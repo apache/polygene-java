f649aa06b26aa754ab1a758889ba5da23c030d4b
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-06-13 08:42:22 +0000

    o Moved API tests to core-tests module. o Added InvalidCompositeException to be thrown if the composite interface is not extending the Composite or if the Composite is a class. o Added tests for that. o Introduced DependencyResolver. o Broke out CompositeContext as interface, to be passed to the DependencyResolver.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@6899 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ tests/src/main/java/org/qi4j/test/model2/StandardThisThatModifier.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2007 Rickard Öberg
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.qi4j.test.model2;
+
+import org.qi4j.api.annotation.Modifies;
+import org.qi4j.api.annotation.Uses;
+
+/**
+ * TODO
+ *
+ */
+public class StandardThisThatModifier
+    implements StandardThat
+{
+    // Attributes ----------------------------------------------------
+    @Uses StandardThis meAsThis;
+    @Modifies StandardThat next;
+
+    // Z implementation ----------------------------------------------
+    public String foo( String aString )
+    {
+        return meAsThis.bar() + "=" + next.foo( aString );
+    }
+
+    public String test( String aString )
+    {
+        return next.test( aString );
+    }
+}
