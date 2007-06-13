670cdeed6ca762e12be4eb34cd92fc7e3e77fb4a
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-06-13 08:32:52 +0000

    o Moved API tests to core-tests module. o Added InvalidCompositeException to be thrown if the composite interface is not extending the Composite or if the Composite is a class. o Added tests for that. o Introduced DependencyResolver. o Broke out CompositeContext as interface, to be passed to the DependencyResolver.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@6896 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ tests/src/test/java/org/qi4j/test/model1/CompositeTest.java
@@ -0,0 +1,179 @@
+/*  Copyright 2007 Niclas Hedhman.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.qi4j.test.model1;
+
+import java.util.ArrayList;
+import java.util.List;
+import junit.framework.TestCase;
+import org.qi4j.api.model.CompositeModel;
+import org.qi4j.api.model.IllegalModifierException;
+import org.qi4j.api.model.MissingModifiesFieldException;
+import org.qi4j.api.model.MixinModel;
+import org.qi4j.api.model.ModifierModel;
+import org.qi4j.api.model.MultipleModifiesFieldException;
+import org.qi4j.api.model.NullArgumentException;
+import org.qi4j.api.model.InvalidCompositeException;
+import org.qi4j.api.strategy.CompositeImpl;
+
+public class CompositeTest extends TestCase
+{
+    public void testComposition1()
+        throws Exception
+    {
+        CompositeModel composite1 = new CompositeModel( Composition1.class );
+        assertEquals( Composition1.class, composite1.getCompositeClass() );
+        List<MixinModel> lists = composite1.getImplementations();
+        assertEquals( 2, lists.size() );
+        MixinModel mixinModel = lists.get( 0 );
+        assertEquals( Mixin1Impl.class, mixinModel.getFragmentClass() );
+        mixinModel = lists.get( 1 );
+        assertEquals( CompositeImpl.class, mixinModel.getFragmentClass() );
+        List<ModifierModel> modifiers1 = composite1.getModifiers();
+        assertEquals( 1, modifiers1.size() );
+        List<MixinModel> mixinModifiers = new ArrayList<MixinModel>();
+        mixinModifiers.add( lists.get( 0 ) );
+        assertEquals( mixinModifiers, composite1.getImplementations( Mixin1.class ) );
+        CompositeModel composite2 = new CompositeModel( Composition1.class );
+        assertEquals( composite1, composite2 );
+        assertEquals( composite1.hashCode(), composite2.hashCode() );
+
+        List<ModifierModel> modifiers2 = lists.get( 0 ).getModifiers();
+        assertEquals( 1, modifiers2.size() );
+        ModifierModel modifier4 = modifiers2.get( 0 );
+        assertEquals( Modifier4.class, modifier4.getFragmentClass() );
+        assertEquals( Modifier4.class.getDeclaredField( "next" ), modifier4.getModifiesField() );
+    }
+
+    public void testComposition2()
+        throws Exception
+    {
+        CompositeModel composite1 = new CompositeModel( Composition2.class );
+        assertEquals( Composition2.class, composite1.getCompositeClass() );
+        List<MixinModel> lists = composite1.getImplementations();
+        assertEquals( 3, lists.size() );
+
+        MixinModel mixin1 = lists.get( 0 );
+        assertEquals( Mixin1Impl.class, mixin1.getFragmentClass() );
+        MixinModel mixin2 = lists.get( 1 );
+        assertEquals( Mixin2Impl.class, mixin2.getFragmentClass() );
+        List<ModifierModel> modifiers1 = composite1.getModifiers();
+        assertEquals( 1, modifiers1.size() );
+        assertEquals( lists.get( 0 ), composite1.getImplementations( Mixin1.class ).get( 0 ) );
+        CompositeModel composite2 = new CompositeModel( Composition2.class );
+        assertEquals( composite1, composite2 );
+        assertEquals( composite1.hashCode(), composite2.hashCode() );
+
+        List<ModifierModel> modifiers3 = mixin2.getModifiers();
+        assertEquals( 0, modifiers3.size() );
+
+        List<ModifierModel> modifiers2 = mixin1.getModifiers();
+        assertEquals( 1, modifiers2.size() );
+        ModifierModel modifier4 = modifiers2.get( 0 );
+        assertEquals( Modifier4.class, modifier4.getFragmentClass() );
+        assertEquals( Modifier4.class.getDeclaredField( "next" ), modifier4.getModifiesField() );
+    }
+
+    // Testing that system check that at least one @Modifies field exist.
+    public void testComposition3()
+        throws Exception
+    {
+        try
+        {
+            CompositeModel composite = new CompositeModel( Composition3.class );
+            fail( "Should throw an MissingModifiesFieldException." );
+        }
+        catch( MissingModifiesFieldException e )
+        {
+            // Expected
+        }
+    }
+
+    // Test that interfaces can't be modifiers.
+    public void testComposition4()
+        throws Exception
+    {
+        try
+        {
+            CompositeModel composite = new CompositeModel( Composition4.class );
+            fail( "Should throw an IllegalModifierException." );
+        }
+        catch( IllegalModifierException e )
+        {
+            // Expected
+        }
+    }
+
+    public void testCompositionNull()
+        throws Exception
+    {
+        try
+        {
+            CompositeModel composite = new CompositeModel( null );
+            fail( "Should throw an NullArgumentException." );
+        }
+        catch( NullArgumentException e )
+        {
+            // Expected
+        }
+    }
+
+    public void testCompositeMultipleNexts()
+        throws Exception
+    {
+        try
+        {
+            CompositeModel composite = new CompositeModel( Composition5.class );
+            fail( "Should throw MultipleModifiesFieldException." );
+        }
+        catch( MultipleModifiesFieldException e )
+        {
+            //Expected
+        }
+    }
+
+    public void testCompositeNotExtendingComposite()
+        throws Exception
+    {
+        try
+        {
+            CompositeModel composite = new CompositeModel( Composition6.class );
+            assertNotNull( composite );
+            fail( "Should throw InvalidCompositeException." );
+        }
+        catch( InvalidCompositeException e )
+        {
+            //Expected
+        }
+
+    }
+
+    public void testCompositeIsNotInterface()
+        throws Exception
+    {
+        try
+        {
+            CompositeModel composite = new CompositeModel( Composition7.class );
+            assertNotNull( composite );
+            fail( "Should throw InvalidCompositeException." );
+        }
+        catch( InvalidCompositeException e )
+        {
+            //Expected
+        }
+
+    }
+}
