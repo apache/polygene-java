bc0f5be2909e710270f91a47fe2e44e2faf87383
siannyhalim@gmail.com <siannyhalim@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-06-21 18:10:32 +0000

    o Add dependency to qi4j-lib-framework in qi4j-tests o Remove dependency to qi4j-tests in qi4j-lib-framework as this will cause cyclic dependency o Add Mixin1 to src/test in qi4j-lib-framework to resolve issue#2 above.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/libraries@7045 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- framework/src/test/java/org/qi4j/library/framework/scripting/ScriptComposite.java
+++ framework/src/test/java/org/qi4j/library/framework/scripting/ScriptComposite.java
@@ -2,7 +2,6 @@ package org.qi4j.library.framework.scripting;
 
 import org.qi4j.api.Composite;
 import org.qi4j.api.annotation.ImplementedBy;
-import org.qi4j.test.model1.Mixin1;
 /*
  * Copyright 2007 Rickard Öberg
  * Licensed under the Apache License, Version 2.0 (the "License"); 
