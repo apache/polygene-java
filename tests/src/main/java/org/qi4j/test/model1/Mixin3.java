505202eb96fcdd6ea8171f1a809816e367104c0d
siannyhalim@gmail.com <siannyhalim@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-06-22 09:47:09 +0000

    Make Mixin3 and Mixin4 to extend Serializable as PropertiesMixin should be used by serializable Mixin.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@7054 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- tests/src/main/java/org/qi4j/test/model1/Mixin3.java
+++ tests/src/main/java/org/qi4j/test/model1/Mixin3.java
@@ -1,10 +1,11 @@
 package org.qi4j.test.model1;
 
+import java.io.Serializable;
 import org.qi4j.api.annotation.ImplementedBy;
 import org.qi4j.library.framework.properties.PropertiesMixin;
 
 @ImplementedBy( { PropertiesMixin.class } )
-public interface Mixin3
+public interface Mixin3 extends Serializable
 {
     void setValue( String value );
 
--- tests/src/main/java/org/qi4j/test/model1/Mixin4.java
+++ tests/src/main/java/org/qi4j/test/model1/Mixin4.java
@@ -1,6 +1,8 @@
 package org.qi4j.test.model1;
 
-public interface Mixin4
+import java.io.Serializable;
+
+public interface Mixin4 extends Serializable
 {
     void setValue( String value );
 
