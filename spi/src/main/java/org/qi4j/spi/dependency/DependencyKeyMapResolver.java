917d0020c4a6d13314fba6572123c1e1313f8bef
rickard.oberg@senselogic.se <rickard.oberg@senselogic.se@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-09-15 14:59:19 +0000

    Massive refactorings, part 2: * Moved around lots of classes to make things more comprehensible * Introduced @Fragment for injection of framework thingies * Introduced ObjectModel/Builder/BuilderFactory to support creation/injection of POJO's. This is to make it easier to integrate Qi4j with existing code and libraries. * Added InjectionKey, as opposed to DependencyKey, and a matcher that can be used to compare keys for injection decisions * Renamed FragmentFactory to InstanceFactory since it should work with POJO's too. * Removed CompositeModelBuilder and moved functionality to CompositeModelFactory * Lots of small fixes
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@8555 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ spi/src/main/java/org/qi4j/spi/dependency/DependencyKeyMapResolver.java
@@ -0,0 +1,78 @@
+/*  Copyright 2007 Niclas Hedhman.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.qi4j.spi.dependency;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.qi4j.api.annotation.scope.Service;
+import org.qi4j.api.model.DependencyKey;
+
+public class DependencyKeyMapResolver
+    implements DependencyResolver, Iterable
+{
+    private Map<DependencyKey, DependencyResolution> dependencies;
+
+    public DependencyKeyMapResolver()
+    {
+        dependencies = new ConcurrentHashMap<DependencyKey, DependencyResolution>();
+    }
+
+    // DependencyResolver implementation ----------------------------
+    public DependencyResolution resolveDependency( DependencyKey key )
+        throws InvalidDependencyException
+    {
+        return dependencies.get( key );
+    }
+
+
+    // Public -------------------------------------------------------
+    public void set( DependencyKey key, Object value )
+    {
+        dependencies.put( key, new StaticDependencyResolution( value ) );
+    }
+
+    public void setService( Class key, Object value )
+    {
+        dependencies.put( new DependencyKey( Service.class, key, null, null ), new StaticDependencyResolution( value ) );
+    }
+
+    public void setService( final String key, Class type, Object value )
+    {
+        dependencies.put( new DependencyKey( Service.class, type, key, null ), new StaticDependencyResolution( value ) );
+    }
+
+    public Object getService( Class key )
+    {
+        return dependencies.get( new DependencyKey( Service.class, key, null, null ) ).getDependencyInjection( null );
+    }
+
+    public Object getService( String key )
+    {
+        return dependencies.get( new DependencyKey( Service.class, null, key, null ) ).getDependencyInjection( null );
+    }
+
+    public Iterator<Map.Entry<DependencyKey, DependencyResolution>> iterator()
+    {
+        return dependencies.entrySet().iterator();
+    }
+
+    public void clear()
+    {
+        dependencies.clear();
+    }
+}
