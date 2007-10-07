df3a03a81d6a08e0a45086221cbb5f42bad2390c
rickard.oberg@senselogic.se <rickard.oberg@senselogic.se@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-10-07 20:31:00 +0000

    * Added better Validatable interface and implementations. Old style removed.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/libraries@8862 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ general/src/main/java/org/qi4j/library/general/model/mixins/ValidatableMixin.java
@@ -0,0 +1,43 @@
+/*  Copyright 2007 Niclas Hedhman.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.qi4j.library.general.model.mixins;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.qi4j.api.annotation.scope.ThisAs;
+import org.qi4j.library.general.model.Validatable;
+import org.qi4j.library.general.model.ValidationException;
+
+public class ValidatableMixin
+    implements Validatable
+{
+    @ThisAs Validatable validatable;
+
+    public List<ValidationMessage> isValid()
+    {
+        return new ArrayList<ValidationMessage>();
+    }
+
+    public void checkValid() throws ValidationException
+    {
+        List<ValidationMessage> messages = validatable.isValid();
+        if( messages.size() > 0 )
+        {
+            throw new ValidationException( messages );
+        }
+    }
+}
