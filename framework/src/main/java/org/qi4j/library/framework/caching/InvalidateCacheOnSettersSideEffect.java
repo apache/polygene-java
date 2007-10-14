d94f5477a6fe11afc45dc7905a0ba811e708e30b
rickard.oberg@senselogic.se <rickard.oberg@senselogic.se@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-10-14 13:42:51 +0000

    Fixed cache clearing on setters
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/libraries@8991 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- framework/src/main/java/org/qi4j/library/framework/caching/InvalidateCacheOnSettersSideEffect.java
+++ framework/src/main/java/org/qi4j/library/framework/caching/InvalidateCacheOnSettersSideEffect.java
@@ -3,6 +3,7 @@ package org.qi4j.library.framework.caching;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import org.qi4j.api.annotation.AppliesTo;
+import org.qi4j.api.annotation.AppliesToFilter;
 import org.qi4j.api.annotation.scope.SideEffectFor;
 import org.qi4j.api.annotation.scope.ThisAs;
 import org.qi4j.library.framework.properties.Setters;
@@ -10,10 +11,24 @@ import org.qi4j.library.framework.properties.Setters;
 /**
  * Invalidate cache on setters.
  */
-@AppliesTo( Setters.class )
+@AppliesTo( InvalidateCacheOnSettersSideEffect.AppliesTo.class )
 public class InvalidateCacheOnSettersSideEffect
     implements InvocationHandler
 {
+    public static class AppliesTo
+        implements AppliesToFilter
+    {
+        public boolean appliesTo( Method method, Class mixin, Class compositeType, Class modifierClass )
+        {
+            if( method.getDeclaringClass().equals( InvocationCache.class ) )
+            {
+                return false;
+            }
+
+            return new Setters().appliesTo( method, mixin, compositeType, modifierClass );
+        }
+    }
+
     @SideEffectFor InvocationHandler next;
     @ThisAs private InvocationCache cache;
 
