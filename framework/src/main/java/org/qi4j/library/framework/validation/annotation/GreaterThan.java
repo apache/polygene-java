c58f63e85b3b1498af9d6ae246297c34940c135e
rickard.oberg@senselogic.se <rickard.oberg@senselogic.se@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-10-15 09:41:01 +0000

    More DbC Constraint annotations
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/libraries@8994 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ framework/src/main/java/org/qi4j/library/framework/validation/annotation/GreaterThan.java
@@ -0,0 +1,14 @@
+package org.qi4j.library.framework.validation.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint
+@Retention( RetentionPolicy.RUNTIME )
+@Target( { ElementType.PARAMETER, ElementType.ANNOTATION_TYPE } )
+public @interface GreaterThan
+{
+    double value();
+}
