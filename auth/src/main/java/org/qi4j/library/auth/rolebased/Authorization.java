22342d0af71085f2839c257992b78e3fc2df8deb
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-01-03 11:43:25 +0000

    package name is wrong.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/libraries@9999 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- auth/src/main/java/org/qi4j/library/auth/rolebased/Authorization.java
+++ auth/src/main/java/org/qi4j/library/auth/rolebased/Authorization.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. 
  */
-package org.qi4j.library.auth.rolebase;
+package org.qi4j.library.auth.rolebased;
 
 import org.qi4j.library.auth.AuthorizationService;
 import org.qi4j.library.auth.AuthorizationManagement;
