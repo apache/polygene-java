38a4ae2f1c61c7a94a9634bd7666fba68448f926
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-01-03 11:10:15 +0000

    Starting a library for authentication and authorization.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/libraries@9992 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ auth/src/main/java/org/qi4j/library/auth/rolebase/Authorization.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2006 Niclas Hedhman.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package org.qi4j.library.auth.rolebase;
+
+import org.qi4j.library.auth.AuthorizationService;
+import org.qi4j.library.auth.AuthorizationManagement;
+import org.qi4j.library.auth.Role;
+import org.qi4j.spi.composite.CompositeBinding;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+public class Authorization
+    implements AuthorizationService, AuthorizationManagement
+{
+    private HashMap<Method, Role[]> methodToRoles;
+
+    public void authorize( CompositeBinding compositeType, Method method, Object[] args )
+        throws SecurityException
+    {
+
+    }
+}
