eb7459ba56ab85e9f5aea6fb01560bb53e6be079
rickardoberg <rickardoberg@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-01-15 05:29:51 +0000

    Moved composite/object builder to ModuleInstance Started EntitySession refactoring Refactored one tutorial to use properties Refactored calendar sample to use properties
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/libraries@10163 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- framework/src/main/java/org/qi4j/library/framework/GenericMixinsAbstractComposite.java
+++ framework/src/main/java/org/qi4j/library/framework/GenericMixinsAbstractComposite.java
@@ -1,6 +1,8 @@
 package org.qi4j.library.framework;
 
 import org.qi4j.composite.Mixins;
+import org.qi4j.library.framework.entity.AssociationMixin;
+import org.qi4j.library.framework.entity.PropertyMixin;
 import org.qi4j.library.framework.properties.PropertiesMixin;
 import org.qi4j.library.framework.scripting.JavaScriptMixin;
 
@@ -8,7 +10,7 @@ import org.qi4j.library.framework.scripting.JavaScriptMixin;
  * Adds all the various generic mixins that can be used to implement
  * Composite methods.
  */
-@Mixins( { PropertiesMixin.class, JavaScriptMixin.class, FinderMixin.class, RMIMixin.class, NoopMixin.class } )
+@Mixins( { PropertyMixin.class, AssociationMixin.class, PropertiesMixin.class, JavaScriptMixin.class, FinderMixin.class, RMIMixin.class, NoopMixin.class } )
 public interface GenericMixinsAbstractComposite
 {
 }
