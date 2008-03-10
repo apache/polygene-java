7d55a3c635e6dbe0d204b8dc02a24b8135550ed0
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-03-10 09:56:54 +0000

    Failing test for authentication concern. Commited for sharing.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@11001 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ testsupport/src/main/java/org/qi4j/test/Qi4jTestMixin.java
@@ -0,0 +1,15 @@
+package org.qi4j.test;
+
+import org.qi4j.composite.Composite;
+import org.qi4j.composite.Mixins;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class Qi4jTestMixin
+    implements InvocationHandler
+{
+    public Object invoke( Object o, Method method, Object[] objects ) throws Throwable
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
