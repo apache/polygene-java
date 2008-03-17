8fc91af284f8a8fa0bdee8232f79d21ab709a421
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-03-17 08:51:34 +0000

    Refactor private mixin test.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@11176 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ tests/src/test/java/org/qi4j/runtime/PrivateMixinTest.java
@@ -0,0 +1,72 @@
+package org.qi4j.runtime;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.qi4j.bootstrap.AssemblyException;
+import org.qi4j.bootstrap.ModuleAssembly;
+import org.qi4j.composite.Composite;
+import org.qi4j.composite.Mixins;
+import org.qi4j.composite.scope.ThisCompositeAs;
+import org.qi4j.test.Qi4jTestSetup;
+
+/**
+ * Unit tests related to private mixins.
+ */
+public class PrivateMixinTest
+    extends Qi4jTestSetup
+{
+    public void assemble( ModuleAssembly module )
+        throws AssemblyException
+    {
+        module.addComposites( SpeakComposite.class );
+    }
+
+    /**
+     * Tests that private mixins are injected correctly.
+     */
+    @Test
+    public void privateMixinInjection()
+    {
+        SpeakComposite test = compositeBuilderFactory.newCompositeBuilder( SpeakComposite.class ).newInstance();
+        assertThat( "Speak", test.speak(), is( equalTo( "I say it works" ) ) );
+    }
+
+    @Mixins( SpeakMixin.class )
+    public interface Speak
+    {
+        String speak();
+    }
+
+    public static class SpeakMixin
+        implements Speak
+    {
+        @ThisCompositeAs Word word;
+
+        public String speak()
+        {
+            return "I say " + word.get();
+        }
+    }
+
+    @Mixins( { WordMixin.class } )
+    public interface SpeakComposite
+        extends Speak, Composite
+    {
+    }
+
+    public interface Word
+    {
+        String get();
+    }
+
+    public static class WordMixin
+        implements Word
+    {
+        public String get()
+        {
+            return "it works";
+        }
+    }
+
+}
