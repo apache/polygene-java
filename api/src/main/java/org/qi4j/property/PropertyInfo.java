41fb312fe7c55fb292e03a42ae8feb20984e1e16
rickardoberg <rickardoberg@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-03-17 06:09:54 +0000

    Added getPropertyType() method to API
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@11166 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- api/src/main/java/org/qi4j/property/PropertyInfo.java
+++ api/src/main/java/org/qi4j/property/PropertyInfo.java
@@ -14,14 +14,45 @@
 
 package org.qi4j.property;
 
+import java.lang.reflect.Type;
+
 /**
- * TODO
+ * Provide access to metadata information about a property.
+ * The {@link Property} interface extends this one, so there
+ * is always easy access to it if you have a reference to a Property.
  */
 public interface PropertyInfo
 {
+    /**
+     * Access metadata about the property with a given type.
+     * The info is registered for the property during
+     * assembly of the application.
+     *
+     * @param infoType the type of the metadata to return
+     * @return a metadata object that implements the requested type or null if none is registered
+     */
     <T> T getPropertyInfo( Class<T> infoType );
 
+    /**
+     * Get the name of the property, which is equal to the name of the metod that declared it.
+     *
+     * @return the name of the property.
+     */
     String getName();
 
+    /**
+     * Get the qualified name of the property which is equal to:<br/>
+     * <interface name>:<method name>
+     *
+     * @return the qualified name of the property
+     */
     String getQualifiedName();
+
+    /**
+     * Get the type of the property. If the property is declared
+     * as Property<X> then X is returned.
+     *
+     * @return the property type
+     */
+    Type getPropertyType();
 }
