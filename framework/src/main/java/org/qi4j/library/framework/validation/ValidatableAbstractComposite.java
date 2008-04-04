1d8fba2f06809e07a5708e2febcc3ea32d51fe14
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-04-04 05:46:15 +0000

    Constraints was sitting in a different change list.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/libraries@11352 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- framework/src/main/java/org/qi4j/library/framework/validation/ValidatableAbstractComposite.java
+++ framework/src/main/java/org/qi4j/library/framework/validation/ValidatableAbstractComposite.java
@@ -6,7 +6,7 @@ import org.qi4j.composite.Mixins;
 /**
  * TODO
  */
-@Concerns( { ValidatableMessagesConcern.class, ConstraintValidationConcern.class, ChangeValidationConcern.class } )
+@Concerns( { ValidatableMessagesConcern.class, ChangeValidationConcern.class } )
 @Mixins( { ValidatableMixin.class, ValidationMessagesMixin.class } )
 public interface ValidatableAbstractComposite
     extends Validatable
