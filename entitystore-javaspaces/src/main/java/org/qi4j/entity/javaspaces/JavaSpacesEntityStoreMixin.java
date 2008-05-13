625e34a1d6c244667c2aa07005fededb7615bf04
jan.kronquist@gmail.com <jan.kronquist@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-05-13 21:14:17 +0000

    Added config file
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/extensions@11718 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- entitystore-javaspaces/src/main/java/org/qi4j/entity/javaspaces/JavaSpacesEntityStoreMixin.java
+++ entitystore-javaspaces/src/main/java/org/qi4j/entity/javaspaces/JavaSpacesEntityStoreMixin.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -204,6 +205,11 @@ public class JavaSpacesEntityStoreMixin implements EntityStore, Activatable
         }
     }
 
+    public Iterator<EntityState> iterator()
+    {
+        throw new UnsupportedOperationException("iterator");
+    }
+
     public void activate() throws Exception
     {
         space = (IJSpace) SpaceFinder.find(config.uri().get());
@@ -213,5 +219,6 @@ public class JavaSpacesEntityStoreMixin implements EntityStore, Activatable
     {
         space = null;
     }
+
 
 }
