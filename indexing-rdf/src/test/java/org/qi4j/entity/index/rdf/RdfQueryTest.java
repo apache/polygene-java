f9b89ed2ba9eca1a556256e25ee2c04fff8ee55f
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-06-15 09:30:13 +0000

    Use addEntities instead of addComposites.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/extensions@11979 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- indexing-rdf/src/test/java/org/qi4j/entity/index/rdf/RdfQueryTest.java
+++ indexing-rdf/src/test/java/org/qi4j/entity/index/rdf/RdfQueryTest.java
@@ -61,7 +61,7 @@ public class RdfQueryTest
         {
             public void assemble( ModuleAssembly module ) throws AssemblyException
             {
-                module.addComposites(
+                module.addEntities(
                     MaleComposite.class,
                     FemaleComposite.class,
                     CityComposite.class,
