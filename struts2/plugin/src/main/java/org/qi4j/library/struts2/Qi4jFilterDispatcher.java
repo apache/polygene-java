201cad56af49a72e2c1c0490f1cc8903a76c2112
rwallace <rwallace@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-08-12 04:22:21 +0000

    refactored and cleaned up
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/libraries@12613 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- struts2/plugin/src/main/java/org/qi4j/library/struts2/Qi4jFilterDispatcher.java
+++ struts2/plugin/src/main/java/org/qi4j/library/struts2/Qi4jFilterDispatcher.java
@@ -12,83 +12,79 @@ import org.qi4j.entity.UnitOfWorkFactory;
 import org.qi4j.object.ObjectBuilderFactory;
 import org.qi4j.structure.Module;
 
+import com.opensymphony.xwork2.config.Configuration;
 import com.opensymphony.xwork2.config.ConfigurationException;
 import com.opensymphony.xwork2.config.ConfigurationManager;
 import com.opensymphony.xwork2.config.ContainerProvider;
 import com.opensymphony.xwork2.inject.ContainerBuilder;
 import com.opensymphony.xwork2.inject.Context;
 import com.opensymphony.xwork2.inject.Factory;
-import com.opensymphony.xwork2.inject.Scope;
 import com.opensymphony.xwork2.util.location.LocatableProperties;
 
 public class Qi4jFilterDispatcher extends FilterDispatcher
-{   
+{
     @Override
     protected Dispatcher createDispatcher( final FilterConfig filterConfig )
     {
         Dispatcher dispatcher = super.createDispatcher( filterConfig );
         ConfigurationManager configurationManager = createConfigurationManager( filterConfig );
-        dispatcher.setConfigurationManager(configurationManager);
+        dispatcher.setConfigurationManager( configurationManager );
         return dispatcher;
     }
 
-    protected ConfigurationManager createConfigurationManager( final FilterConfig filterConfig )
+    protected ConfigurationManager createConfigurationManager(FilterConfig filterConfig )
     {
         ConfigurationManager configurationManager = new ConfigurationManager( BeanSelectionProvider.DEFAULT_BEAN_NAME );
-        configurationManager.addContainerProvider(new ContainerProvider() {
-            private boolean registered = false;
-            
-            public void register(ContainerBuilder builder, LocatableProperties props) throws ConfigurationException {
-                builder.factory(UnitOfWorkFactory.class, new Factory<UnitOfWorkFactory>() {
-                    public UnitOfWorkFactory create(Context context) throws Exception {
-                        return getUnitOfWorkFactory(filterConfig);
-                    }
-                }, Scope.SINGLETON);
-                builder.factory(ObjectBuilderFactory.class, new Factory<ObjectBuilderFactory>() {
-                    public ObjectBuilderFactory create(Context context) throws Exception {
-                        return getObjectBuilderFactory(filterConfig);
-                    }
-                }, Scope.SINGLETON);
-                builder.factory(CompositeBuilderFactory.class, new Factory<CompositeBuilderFactory>() {
-                    public CompositeBuilderFactory create(Context context) throws Exception {
-                        return getCompositeBuilderFactory(filterConfig);
-                    }
-                }, Scope.SINGLETON);
-                builder.factory(ActionConfiguration.class, new Factory<ActionConfiguration>() {
-                    public ActionConfiguration create(Context context) throws Exception {
-                        return getActionConfiguration(filterConfig);
-                    }
-                }, Scope.SINGLETON);
-                registered = true;
-            }
-
-            public boolean needsReload() {
-                return !registered;
-            }
-            
-            public void init(com.opensymphony.xwork2.config.Configuration configuration) throws ConfigurationException {}
-            public void destroy() {}
-        });
+        configurationManager.addContainerProvider( new Qi4jContainerProvider( module( filterConfig ) ) );
         return configurationManager;
     }
-        
-    private UnitOfWorkFactory getUnitOfWorkFactory(FilterConfig filterConfig ) {
-        return getModule(filterConfig).unitOfWorkFactory();
-    }
-    
-    private ObjectBuilderFactory getObjectBuilderFactory(FilterConfig filterConfig) {
-        return getModule(filterConfig).objectBuilderFactory();
-    }
 
-    private CompositeBuilderFactory getCompositeBuilderFactory(FilterConfig filterConfig) {
-        return getModule(filterConfig).compositeBuilderFactory();
-    }
-    
-    private ActionConfiguration getActionConfiguration(FilterConfig filterConfig) {
-        return getModule(filterConfig).serviceFinder().findService( ActionService.class ).metaInfo( ActionConfiguration.class );
-    }
-
-    private Module getModule(FilterConfig filterConfig) {
+    private Module module( FilterConfig filterConfig )
+    {
         return (Module) filterConfig.getServletContext().getAttribute( SERVLET_ATTRIBUTE );
+    }
+
+    class Qi4jContainerProvider implements ContainerProvider
+    {
+        private final Module module;
+        private boolean registered = false;
+
+        Qi4jContainerProvider( Module aModule )
+        {
+            module = aModule;
+        }
+
+        public void register( ContainerBuilder builder, LocatableProperties props ) throws ConfigurationException
+        {
+            factory( builder, UnitOfWorkFactory.class, module.unitOfWorkFactory() );
+            factory( builder, ObjectBuilderFactory.class, module.objectBuilderFactory() );
+            factory( builder, CompositeBuilderFactory.class, module.compositeBuilderFactory() );
+            factory( builder, ActionConfiguration.class, actionConfiguration() );
+            registered = true;
+        }
+
+        public boolean needsReload()
+        {
+            return !registered;
+        }
+
+        public void init(Configuration configuration ) throws ConfigurationException {}
+        public void destroy() {}
+        
+        private <T> void factory( ContainerBuilder builder, Class<T> type, final T value )
+        {
+            builder.factory( type, new Factory<T>()
+            {
+                public T create( Context context )
+                {
+                    return value;
+                }
+            });
+        }
+        
+        private ActionConfiguration actionConfiguration()
+        {
+            return module.serviceFinder().findService( ActionService.class ).metaInfo( ActionConfiguration.class );
+        }
     }
 }
