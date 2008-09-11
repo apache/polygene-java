6f38689be7fe9df93d68021e02dc72a47026fee2
rwallace <rwallace@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-09-11 22:20:58 +0000

    fix example app
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/libraries@12733 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- struts2/example/src/main/java/org/qi4j/library/struts2/example/actions/ListItems.java
+++ struts2/example/src/main/java/org/qi4j/library/struts2/example/actions/ListItems.java
@@ -6,14 +6,13 @@ import org.qi4j.composite.Composite;
 import org.qi4j.composite.Mixins;
 import org.qi4j.library.struts2.example.Item;
 import org.qi4j.library.struts2.support.list.ListOf;
+import org.qi4j.library.struts2.support.list.ProvidesListOf;
 import org.qi4j.library.struts2.support.list.ProvidesListOfMixin;
-
-import com.opensymphony.xwork2.Action;
 
 @ListOf(Item.class)
 @Results({
     @Result(name="success", value="/jsp/listItems.jsp")
 })
 @Mixins(ProvidesListOfMixin.class)
-public interface ListItems extends Action, Composite {
+public interface ListItems extends ProvidesListOf<Item>, Composite {
 }
