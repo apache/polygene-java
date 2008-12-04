5af945cd7e2613d47457954dc7772e13b4bdbce2
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-12-04 09:48:55 +0000

    Commit 16. ----------
    o EntitySerializer is added in the Assemblers.
    
    o License header corrections.
    
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/libraries@13498 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- rdf/src/main/java/org/qi4j/library/rdf/parse/model/LayerParser.java
+++ rdf/src/main/java/org/qi4j/library/rdf/parse/model/LayerParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 Niclas Hedhman.
+ * Copyright 2007, 2008 Niclas Hedhman.
  *
  * Licensed  under the  Apache License,  Version 2.0  (the "License");
  * you may not use  this file  except in  compliance with the License.
