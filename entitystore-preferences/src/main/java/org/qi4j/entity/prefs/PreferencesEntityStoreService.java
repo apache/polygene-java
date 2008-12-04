763b8a9f5a179e22a220e10f069f24b9d4530095
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-12-04 09:40:56 +0000

    Commit 12. ----------
    o Added a utility so one can see what is in the Preferences.
    
    o Added an Assembler.
    
    o Added "instance version" support (not complete).
    
    o Fixed that the TestCase cleans up the Preferences afterwards.
    
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/extensions@13494 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ entitystore-preferences/src/main/java/org/qi4j/entity/prefs/PreferencesEntityStoreService.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2008, Rickard Öberg. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.qi4j.entity.prefs;
+
+import org.qi4j.composite.Concerns;
+import org.qi4j.composite.Mixins;
+import org.qi4j.composite.SideEffects;
+import org.qi4j.entity.memory.ConcurrentModificationCheckConcern;
+import org.qi4j.service.Activatable;
+import org.qi4j.service.Configuration;
+import org.qi4j.service.ServiceComposite;
+import org.qi4j.spi.entity.EntityStore;
+import org.qi4j.spi.entity.EntityStoreListenerNotificationSideEffect;
+
+/**
+ * EntityStore backed by Preferences API.
+ * <p/>
+ * A root node is created in the System preferences, whose name
+ * is the same as the Application name (default:"Application").
+ * <p/>
+ * Each entity is stored under its identity name.
+ * <p/>
+ * Property types are converted to native Preferences API types
+ * as much as possible. All others will be serialized to a byte array.
+ * <p/>
+ * ManyAssociations are stored as multi-line strings (one QualifiedIdentity
+ * per line), and Associations are stored as the stringified QualifiedIdentity
+ * of the referenced Entity.
+ * <p/>
+ * The main use of the EntityStore is for storage of ServiceComposite Configurations.
+ *
+ * @see ServiceComposite
+ * @see Configuration
+ */
+@Concerns( ConcurrentModificationCheckConcern.class )
+@SideEffects( EntityStoreListenerNotificationSideEffect.class )
+@Mixins( PreferencesEntityStoreMixin.class )
+public interface PreferencesEntityStoreService
+    extends EntityStore, ServiceComposite, Activatable
+{
+}
---
+++ entitystore-preferences/src/test/java/org/qi4j/entity/prefs/PreferencesEntityStoreTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2008, Rickard Öberg. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.qi4j.entity.prefs;
+
+import java.util.prefs.Preferences;
+import org.junit.After;
+import org.qi4j.bootstrap.AssemblyException;
+import org.qi4j.bootstrap.ModuleAssembly;
+import static org.qi4j.entity.prefs.PreferenceEntityStoreInfo.PreferenceNode.USER_ROOT;
+import org.qi4j.test.entity.AbstractEntityStoreTest;
+
+/**
+ * TODO
+ */
+public class PreferencesEntityStoreTest extends AbstractEntityStoreTest
+{
+    public void assemble( ModuleAssembly module )
+        throws AssemblyException
+    {
+        super.assemble( module );
+
+        module.addServices( PreferencesEntityStoreService.class )
+            .setMetaInfo( new PreferenceEntityStoreInfo( USER_ROOT ) )
+            .instantiateOnStartup();
+    }
+
+    @Override
+    @After 
+    public void tearDown()
+        throws Exception
+    {
+        super.tearDown();
+        Preferences preferences = Preferences.userRoot();
+        preferences = preferences.node( "/" );
+        preferences = preferences.node( "Application" );
+        preferences.removeNode();
+    }
+}
