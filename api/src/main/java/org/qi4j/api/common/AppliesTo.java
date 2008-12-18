6ba353445cdcb26a6f805d6a6767ea8bed1f11ce
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-12-18 12:00:10 +0000

    o Formatting for documentation purposes.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/core@13793 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- api/src/main/java/org/qi4j/api/common/AppliesTo.java
+++ api/src/main/java/org/qi4j/api/common/AppliesTo.java
@@ -4,12 +4,14 @@
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
  */
 
 package org.qi4j.api.common;
@@ -22,8 +24,8 @@ import java.lang.annotation.Target;
 
 /**
  * Modifiers that implement InvocationHandler and which should only
- * be applied to methods that have a particular annotation or implement
- * a known interface should use this annotation.
+ * be applied to methods that have a particular annotation or
+ * implement a known interface should use this annotation.
  * <p>
  * Example:
  * </p>
@@ -32,7 +34,9 @@ import java.lang.annotation.Target;
  * public class SessionModifier
  * implements InvocationHandler
  * {
- *     public Object invoke( Object proxy, Method method, Object[] args )
+ *     public Object invoke( Object proxy,
+ *                           Method method, 
+ *                           Object[] args )
  *         throws Throwable
  *     {
  *         ... do session stuff ...
@@ -50,12 +54,12 @@ import java.lang.annotation.Target;
  * {
  *     private State state;
  *
- *     @Sessional public void setSomeState( State state )
+ *     &#64;Sessional public void setSomeState( State state )
  *     {
  *         this.state = state;
  *     }
  *
- *     @Sessional public State getSomeState()
+ *     &#64;Sessional public State getSomeState()
  *     {
  *         return this.state;
  *     }
@@ -83,9 +87,10 @@ import java.lang.annotation.Target;
  * {}
  * </code></pre>
  * <p>
- * The setStateService and getStateService methods do not have the @Sessional annotation,
- * therefore the SessionModifier will not be placed into the call sequence of these
- * methods, and vice-versa.
+ * The setStateService and getStateService methods do not have the
+ * &#64;Sessional annotation, therefore the SessionModifier will
+ * not be placed into the call sequence of these methods, and
+ * vice-versa.
  * </p>
  */
 @Retention( RetentionPolicy.RUNTIME )
@@ -93,9 +98,10 @@ import java.lang.annotation.Target;
 @Documented
 public @interface AppliesTo
 {
-    // List of interfaces, annotations or AppliesToFilter implementation classes
-    // If one of them matches the current element it will be accepted,
-    // so this list can be considered an "or"
-
+    /** List of interfaces, annotations or AppliesToFilter
+     *implementation classes.
+     * If one of them matches the current element it will be
+     * accepted, so this list can be considered an "or".
+     */
     Class<?>[] value();
 }
