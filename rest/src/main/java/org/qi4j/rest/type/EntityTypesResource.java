7e7c58b6f04cd0b8695e86466037d75312b8123f
edward.yakop@gmail.com <edward.yakop@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-12-22 15:57:54 +0000

    o Add #representHTML for QI-160.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/extensions@13861 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- rest/src/main/java/org/qi4j/rest/type/EntityTypesResource.java
+++ rest/src/main/java/org/qi4j/rest/type/EntityTypesResource.java
@@ -13,32 +13,110 @@
  */
 package org.qi4j.rest.type;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.qi4j.api.composite.Composite;
+import org.qi4j.api.injection.scope.Structure;
 import org.qi4j.api.injection.scope.Uses;
+import org.qi4j.api.structure.Application;
+import org.qi4j.api.structure.Module;
+import org.qi4j.spi.Qi4jSPI;
+import org.qi4j.spi.entity.EntityDescriptor;
+import org.qi4j.spi.structure.ApplicationSPI;
+import org.qi4j.spi.structure.DescriptorVisitor;
 import org.restlet.Context;
+import static org.restlet.data.CharacterSet.UTF_8;
 import org.restlet.data.MediaType;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
+import org.restlet.data.Status;
 import org.restlet.resource.Representation;
 import org.restlet.resource.Resource;
 import org.restlet.resource.ResourceException;
-import org.restlet.resource.StringRepresentation;
 import org.restlet.resource.Variant;
+import org.restlet.resource.WriterRepresentation;
 
-public class EntityTypesResource extends Resource
+public final class EntityTypesResource extends Resource
 {
-    public EntityTypesResource( @Uses Context context, @Uses Request request, @Uses Response response )
+    private final Set<String> entityTypes;
+
+    public EntityTypesResource( @Uses Context context,
+                                @Uses Request request,
+                                @Uses Response response,
+                                @Structure Application application,
+                                final @Structure Qi4jSPI spi,
+                                final @Structure Module module )
     {
         super( context, request, response );
 
-        getVariants().add( new Variant( MediaType.TEXT_PLAIN ) );
+        List<Variant> variants = getVariants();
+        variants.add( new Variant( MediaType.TEXT_HTML ) );
+
+        entityTypes = new HashSet<String>();
+        // Get all entity types
+        ApplicationSPI applicationSPI = (ApplicationSPI) application;
+        applicationSPI.visitDescriptor( new DescriptorVisitor()
+        {
+            @Override public void visit( EntityDescriptor entityDescriptor )
+            {
+                Class<? extends Composite> entityType = entityDescriptor.type();
+                if( spi.getEntityDescriptor( entityType, module ) != null )
+                {
+                    entityTypes.add( entityType.getName() );
+                }
+            }
+        } );
     }
 
     @Override
-    public Representation represent( Variant variant ) throws ResourceException
+    public Representation represent( Variant variant )
+        throws ResourceException
     {
-        Representation representation = new StringRepresentation(
-            "hello, world", MediaType.TEXT_PLAIN );
-        return representation;
+        if( MediaType.TEXT_HTML.equals( variant.getMediaType() ) )
+        {
+            return representHTML( entityTypes );
+        }
+
+        throw new ResourceException( Status.CLIENT_ERROR_NOT_FOUND );
     }
 
+    private Representation representHTML( final Set<String> entityTypes )
+    {
+        Representation representation = new WriterRepresentation( MediaType.TEXT_HTML )
+        {
+            public void write( Writer buf )
+                throws IOException
+            {
+                PrintWriter out = new PrintWriter( buf );
+
+                String path = getRequest().getResourceRef().getPath();
+                if( !path.endsWith( "/" ) )
+                {
+                    path += "/";
+                }
+
+                try
+                {
+                    out.println( "<html><head><title>All entities</title</head><body><h1>All entities</h1><ul>" );
+
+                    for( String entityType : entityTypes )
+                    {
+                        out.println( "<li><a href=\"" + path + entityType + ".html\">" + entityType + "</a></li>" );
+                    }
+
+                    out.println( "</ul></body></html>" );
+                }
+                finally
+                {
+                    out.close();
+                }
+            }
+        };
+        representation.setCharacterSet( UTF_8 );
+        return representation;
+    }
 }
