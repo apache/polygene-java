1d852ed89eda3139c8a6e80656175d32cb0425ee
niclas@hedhman.org <niclas@hedhman.org@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-03-04 07:11:23 +0000

    o Fixes in Native Sesame store.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/qi4j/extensions@14330 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- indexing-rdf/src/main/java/org/qi4j/index/rdf/assembly/RdfNativeSesameStoreAssembler.java
+++ indexing-rdf/src/main/java/org/qi4j/index/rdf/assembly/RdfNativeSesameStoreAssembler.java
@@ -19,6 +19,7 @@ import org.qi4j.bootstrap.ModuleAssembly;
 import org.qi4j.index.rdf.RdfFactoryService;
 import org.qi4j.index.rdf.RdfQueryService;
 import org.qi4j.library.rdf.entity.EntityStateSerializer;
+import org.qi4j.library.rdf.entity.EntityTypeSerializer;
 import org.qi4j.library.rdf.repository.NativeConfiguration;
 import org.qi4j.library.rdf.repository.NativeRepositoryService;
 
@@ -32,6 +33,6 @@ public class RdfNativeSesameStoreAssembler
         module.addServices( NativeRepositoryService.class ).instantiateOnStartup().identifiedBy( "rdf-indexing" );
         module.addServices( RdfQueryService.class ).instantiateOnStartup();
         module.addServices( RdfFactoryService.class );
-        module.addObjects( EntityStateSerializer.class );
+        module.addObjects( EntityStateSerializer.class, EntityTypeSerializer.class );
     }
 }
