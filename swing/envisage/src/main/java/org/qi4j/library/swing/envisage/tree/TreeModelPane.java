0be210a3ebbe8dee977cae8b75d6ce328ad6828b
tonny <tonny@blue.(none)>
2009-04-02 21:54:05 +0700

    Update TreeModelPane

--- swing/envisage/src/main/java/org/qi4j/library/swing/envisage/tree/TreeModelPane.java
+++ swing/envisage/src/main/java/org/qi4j/library/swing/envisage/tree/TreeModelPane.java
@@ -16,28 +16,16 @@
 */
 package org.qi4j.library.swing.envisage.tree;
 
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
+import org.qi4j.library.swing.envisage.model.descriptor.ApplicationDetailDescriptor;
+
+import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.*;
+import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.ResourceBundle;
-import javax.swing.BorderFactory;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTree;
-import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.MutableTreeNode;
-import javax.swing.tree.TreeNode;
-import javax.swing.tree.TreePath;
-import org.qi4j.library.swing.envisage.model.descriptor.ApplicationDetailDescriptor;
 
 /**
  * Application Model View as Swing Component.
@@ -188,6 +176,10 @@ public class TreeModelPane extends JPanel
             TreePath treePath = new TreePath( treeModel.getPathToRoot( node ) );
             structureTree.setSelectionPath( treePath );
             structureTree.scrollPathToVisible( treePath );
+        }
+        else
+        {
+            structureTree.clearSelection();
         }
     }
 
