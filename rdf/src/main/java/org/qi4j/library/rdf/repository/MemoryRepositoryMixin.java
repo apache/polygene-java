77d6b09905b19803f9c120d6fd7158bcf982d760
Niclas Hedhman <niclas@hedhman.org>
2009-10-10 17:25:06 +0800

    o Resetting possibility of the RDF repository.

--- rdf/src/main/java/org/qi4j/library/rdf/repository/MemoryRepositoryMixin.java
+++ rdf/src/main/java/org/qi4j/library/rdf/repository/MemoryRepositoryMixin.java
@@ -23,21 +23,23 @@ import org.openrdf.sail.memory.MemoryStore;
 import org.qi4j.api.service.Activatable;
 
 public class MemoryRepositoryMixin
-    implements Repository, Activatable
+    implements Repository, ResetableRepository, Activatable
 {
     SailRepository repo;
 
     public MemoryRepositoryMixin()
     {
-        repo = new SailRepository(new MemoryStore() );
+        repo = new SailRepository( new MemoryStore() );
     }
 
-    public void activate() throws Exception
+    public void activate()
+        throws Exception
     {
         repo.initialize();
     }
 
-    public void passivate() throws Exception
+    public void passivate()
+        throws Exception
     {
         repo.shutDown();
     }
@@ -52,20 +54,24 @@ public class MemoryRepositoryMixin
         return repo.getDataDir();
     }
 
-    public void initialize() throws RepositoryException
+    public void initialize()
+        throws RepositoryException
     {
     }
 
-    public void shutDown() throws RepositoryException
+    public void shutDown()
+        throws RepositoryException
     {
     }
 
-    public boolean isWritable() throws RepositoryException
+    public boolean isWritable()
+        throws RepositoryException
     {
         return repo.isWritable();
     }
 
-    public RepositoryConnection getConnection() throws RepositoryException
+    public RepositoryConnection getConnection()
+        throws RepositoryException
     {
         return repo.getConnection();
     }
@@ -73,5 +79,11 @@ public class MemoryRepositoryMixin
     public ValueFactory getValueFactory()
     {
         return repo.getValueFactory();
+    }
+
+    public void discardEntireRepository()
+        throws RepositoryException
+    {
+        repo = new SailRepository( new MemoryStore() );
     }
 }
