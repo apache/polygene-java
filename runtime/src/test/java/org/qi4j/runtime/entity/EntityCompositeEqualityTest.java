466fe34b81f71a9013e9764a2d5eeda0bed4c148
Niclas Hedhman <niclas@hedhman.org>
2009-10-11 14:48:38 +0800

    Moved EntityCompositeEqualityTest to Core from qi4j-tests

---
+++ runtime/src/test/java/org/qi4j/runtime/entity/EntityCompositeEqualityTest.java
@@ -0,0 +1,74 @@
+package org.qi4j.runtime.entity;
+
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.After;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.qi4j.api.common.Visibility;
+import org.qi4j.api.entity.EntityBuilder;
+import org.qi4j.api.entity.EntityComposite;
+import org.qi4j.api.unitofwork.UnitOfWork;
+import org.qi4j.api.unitofwork.UnitOfWorkCompletionException;
+import org.qi4j.bootstrap.AssemblyException;
+import org.qi4j.bootstrap.ModuleAssembly;
+import org.qi4j.entitystore.memory.MemoryEntityStoreService;
+import org.qi4j.spi.uuid.UuidIdentityGeneratorService;
+import org.qi4j.test.AbstractQi4jTest;
+
+/**
+ * Testing of equality of entity composites.
+ */
+public class EntityCompositeEqualityTest
+    extends AbstractQi4jTest
+{
+    private UnitOfWork unitOfWork;
+    private EntityBuilder<MyComposite> myCompositeBuilder;
+
+    @Before
+    @Override
+    public void setUp()
+        throws Exception
+    {
+        super.setUp();
+        unitOfWork = this.unitOfWorkFactory.newUnitOfWork();
+        myCompositeBuilder = unitOfWork.newEntityBuilder( MyComposite.class );
+    }
+
+    @After
+    public void completeUnitOfWork()
+        throws Exception
+    {
+        unitOfWork.complete();
+        super.tearDown();
+    }
+
+    @Test
+    public void shouldNotBeEqualToNull()
+        throws UnitOfWorkCompletionException
+    {
+        MyComposite simpleComposite = myCompositeBuilder.newInstance();
+        assertThat( "simpleComposite is not equal to null", simpleComposite.equals( null ), equalTo( false ) );
+    }
+
+    @Test
+    public void shouldBeEqualToItself()
+    {
+        MyComposite simpleComposite = myCompositeBuilder.newInstance();
+        assertThat( "simple composite is equal to itself", simpleComposite.equals( simpleComposite ), equalTo( true ) );
+    }
+
+    public void assemble( ModuleAssembly module )
+        throws AssemblyException
+    {
+        module.addServices( UuidIdentityGeneratorService.class, MemoryEntityStoreService.class )
+            .visibleIn( Visibility.layer );
+        module.addObjects( EntityCompositeEqualityTest.class );
+        module.addEntities( MyComposite.class );
+    }
+
+    private static interface MyComposite
+        extends EntityComposite
+    {
+    }
+}
