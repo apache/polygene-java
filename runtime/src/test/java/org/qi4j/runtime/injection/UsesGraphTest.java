453a343ed4b78c73426b4de55824ef168bcff14b
Niclas Hedhman <niclas@hedhman.org>
2009-10-11 13:53:46 +0800

    Moved UsesGraphTest to Core from qi4j-tests

---
+++ runtime/src/test/java/org/qi4j/runtime/injection/UsesGraphTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2009, Rickard Öberg. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.qi4j.runtime.injection;
+
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Assert;
+import org.junit.Test;
+import org.qi4j.api.injection.scope.Uses;
+import org.qi4j.api.object.ObjectBuilder;
+import org.qi4j.bootstrap.AssemblyException;
+import org.qi4j.bootstrap.ModuleAssembly;
+import org.qi4j.test.AbstractQi4jTest;
+
+/**
+ * Test creation of object graph, where @Uses is used to refer to instances
+ */
+public class UsesGraphTest
+    extends AbstractQi4jTest
+{
+    public void assemble( ModuleAssembly module )
+        throws AssemblyException
+    {
+        module.addObjects( A.class, B.class, C.class, D.class );
+    }
+
+    @Test
+    public void givenGraphDependenciesWhenInstantiateAThenGetSameReferences()
+    {
+        A a = objectBuilderFactory.newObject( A.class );
+
+        Assert.assertThat( "Same reference", a.c, equalTo( a.b.c ) );
+        Assert.assertThat( "Same reference", a.d, equalTo( a.b.c.d ) );
+    }
+
+    @Test
+    public void givenGraphDependenciesWhenInstantiateUsingBuildersThenDontGetSameReferences()
+    {
+        ObjectBuilder<A> builder = objectBuilderFactory.newObjectBuilder( A.class );
+        builder.use( objectBuilderFactory.newObjectBuilder( C.class ) );
+        A a = builder.newInstance();
+        Assert.assertThat( "Not same reference", a.c, not( equalTo( a.b.c ) ) );
+        Assert.assertThat( "Not same reference", a.d, not( equalTo( a.b.c.d ) ) );
+    }
+
+    static public class A
+    {
+        @Uses
+        B b;
+
+        @Uses
+        C c;
+
+        @Uses
+        D d;
+    }
+
+    static public class B
+    {
+        @Uses
+        C c;
+    }
+
+    static public class C
+    {
+        @Uses
+        D d;
+    }
+
+    static public class D
+    {
+
+    }
+}
