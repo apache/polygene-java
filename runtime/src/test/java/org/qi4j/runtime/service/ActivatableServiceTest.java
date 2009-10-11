864f538d3c44e4aa32c5808164d524d9c1fad6b4
Niclas Hedhman <niclas@hedhman.org>
2009-10-11 14:14:42 +0800

    Moved ActivatableServiceTest to Core from qi4j-tests

---
+++ runtime/src/test/java/org/qi4j/runtime/service/ActivatableServiceTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2008, Rickard Öberg. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.qi4j.runtime.service;
+
+import junit.framework.TestCase;
+import org.qi4j.api.injection.scope.Service;
+import org.qi4j.api.mixin.Mixins;
+import org.qi4j.api.service.Activatable;
+import org.qi4j.api.service.ServiceComposite;
+import org.qi4j.api.service.ServiceReference;
+import org.qi4j.bootstrap.AssemblyException;
+import org.qi4j.bootstrap.ModuleAssembly;
+import org.qi4j.bootstrap.SingletonAssembler;
+
+/**
+ * Test of activatable services
+ */
+public class ActivatableServiceTest
+    extends TestCase
+{
+    @Service
+    ServiceReference<Activatable> service;
+
+    public static boolean isActive;
+
+    public void testActivatable()
+        throws Exception
+    {
+        SingletonAssembler assembly = new SingletonAssembler()
+        {
+            public void assemble( ModuleAssembly module ) throws AssemblyException
+            {
+                module.addObjects( ActivatableServiceTest.class );
+                module.addServices( ActivatableComposite.class ).instantiateOnStartup();
+            }
+        };
+
+        assertTrue( isActive );
+
+        assembly.objectBuilderFactory().newObjectBuilder( ActivatableServiceTest.class ).injectTo( this );
+
+        assertTrue( isActive );
+
+        service.get();
+
+        assertTrue( isActive );
+
+        assembly.application().passivate();
+
+        assertFalse( isActive );
+    }
+
+    @Mixins( ActivatableMixin.class )
+    public static interface ActivatableComposite
+        extends Activatable, ServiceComposite
+    {
+    }
+
+    public static class ActivatableMixin
+        implements Activatable
+    {
+        public void activate() throws Exception
+        {
+            isActive = true;
+        }
+
+        public void passivate() throws Exception
+        {
+            if( !isActive )
+            {
+                throw new Exception( "Not active!" );
+            }
+
+            isActive = false;
+        }
+    }
+}
