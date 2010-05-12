819b1bcac3d3dfffc679f493fb43cde48e83035b
Stanislav Muhametsin <stanislav.muhametsin@tut.fi>
2010-05-12 03:53:04 +0300

    Removed the previous sql indexing project.
    Additionally fixed some small errors here and there. Still haven't managed to do maven build or install onto this one, but getting close.

--- extensions/indexing-sql/src/test/java/org/qi4j/index/sql/SqlEntityFinderTest.java
+++ extensions/indexing-sql/src/test/java/org/qi4j/index/sql/SqlEntityFinderTest.java
@@ -1,34 +1,44 @@
 /*
- * Copyright 2008 Alin Dreghiciu.
+ * Copyright (c) 2010, Stanislav Muhametsin. All Rights Reserved.
  *
- * Licensed  under the  Apache License,  Version 2.0  (the "License");
- * you may not use  this file  except in  compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed  under the  License is distributed on an "AS IS" BASIS,
- * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
- * implied.
- *
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
+
+
 package org.qi4j.index.sql;
 
-import org.junit.Ignore;
+import org.qi4j.api.common.Visibility;
 import org.qi4j.bootstrap.AssemblyException;
 import org.qi4j.bootstrap.ModuleAssembly;
+import org.qi4j.entitystore.memory.MemoryEntityStoreService;
+import org.qi4j.index.sql.RDBConfiguration;
+import org.qi4j.index.sql.assembly.SQLIndexingAssembler;
+import org.qi4j.spi.uuid.UuidIdentityGeneratorService;
 import org.qi4j.test.indexing.AbstractEntityFinderTest;
 
-@Ignore
-public class SqlEntityFinderTest
-    extends AbstractEntityFinderTest
+/**
+ *
+ * @author Stanislav Muhametsin
+ *
+ */
+public class SQLEntityFinderTest extends AbstractEntityFinderTest
 {
-    @Override
-    protected void setupTest( ModuleAssembly module )
-        throws AssemblyException
-    {
-    }
+ 
+   @Override
+   protected void setupTest(ModuleAssembly mainModule) throws AssemblyException
+   {
+      SQLIndexingAssembler ass = new SQLIndexingAssembler();
+      ass.assemble(mainModule);
+      
+      mainModule.addServices(MemoryEntityStoreService.class, UuidIdentityGeneratorService.class).visibleIn(Visibility.application);
+      mainModule.addEntities(RDBConfiguration.class).visibleIn(Visibility.application);
+   }
 }
--- extensions/indexing-sql/src/test/java/org/qi4j/index/sql/SqlQueryTest.java
+++ extensions/indexing-sql/src/test/java/org/qi4j/index/sql/SqlQueryTest.java
@@ -1,41 +1,59 @@
 /*
- * Copyright 2008 Alin Dreghiciu.
- * Copyright 2009 Niclas Hedhman.
+ * Copyright (c) 2010, Stanislav Muhametsin. All Rights Reserved.
  *
- * Licensed  under the  Apache License,  Version 2.0  (the "License");
- * you may not use  this file  except in  compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed  under the  License is distributed on an "AS IS" BASIS,
- * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
- * implied.
- *
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
+
+
 package org.qi4j.index.sql;
 
-import org.junit.Ignore;
+import org.qi4j.api.common.Visibility;
+import org.qi4j.bootstrap.ApplicationAssembly;
 import org.qi4j.bootstrap.AssemblyException;
+import org.qi4j.bootstrap.AssemblyVisitor;
+import org.qi4j.bootstrap.EntityDeclaration;
+import org.qi4j.bootstrap.ImportedServiceDeclaration;
+import org.qi4j.bootstrap.LayerAssembly;
 import org.qi4j.bootstrap.ModuleAssembly;
+import org.qi4j.bootstrap.ObjectDeclaration;
+import org.qi4j.bootstrap.ServiceDeclaration;
+import org.qi4j.bootstrap.TransientDeclaration;
+import org.qi4j.bootstrap.ValueDeclaration;
+import org.qi4j.entitystore.memory.MemoryEntityStoreService;
+import org.qi4j.index.sql.RDBConfiguration;
+import org.qi4j.index.sql.assembly.SQLIndexingAssembler;
+import org.qi4j.spi.structure.ApplicationSPI;
+import org.qi4j.spi.uuid.UuidIdentityGeneratorService;
 import org.qi4j.test.indexing.AbstractQueryTest;
 
-@Ignore
-public class SqlQueryTest
-    extends AbstractQueryTest
+/**
+ *
+ * @author Stanislav Muhametsin
+ */
+public class SQLQueryTest extends AbstractQueryTest
 {
 
-    @Override
-    protected void setupTest( ModuleAssembly module )
-        throws AssemblyException
-    {
-    }
+   @Override
+   protected void setupTest(ModuleAssembly mainModule) throws AssemblyException
+   {
+      SQLIndexingAssembler ass = new SQLIndexingAssembler();
+      ass.assemble(mainModule);
+      
+      mainModule.addServices(MemoryEntityStoreService.class, UuidIdentityGeneratorService.class).visibleIn(Visibility.application);
+      mainModule.addEntities(RDBConfiguration.class).visibleIn(Visibility.application);
+   }
+   
+   @Override
+   protected void tearDownTest()
+   {
+   }
 
-    @Override
-    protected void tearDownTest()
-    {
-    }
 }
