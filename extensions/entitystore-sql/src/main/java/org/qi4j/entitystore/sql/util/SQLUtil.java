dc2d52200a4333de13580cc7fb29df2ef02a6627
Paul MERLIN <paul@nosphere.org>
2010-06-19 15:25:01 +0200

    entitystore-sql: fixed source headers, added gitignore

--- extensions/entitystore-sql/src/main/java/org/qi4j/entitystore/sql/util/SQLUtil.java
+++ extensions/entitystore-sql/src/main/java/org/qi4j/entitystore/sql/util/SQLUtil.java
@@ -1,17 +1,15 @@
 /*
- * Created on 7 juin 2010
+ * Copyright (c) 2010, Paul Merlin. All Rights Reserved.
  *
- * Licenced under the Netheos Licence, Version 1.0 (the "Licence"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at :
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- * http://www.netheos.net/licences/LICENCE-1.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- *
- * Copyright (c) Netheos
  */
 package org.qi4j.entitystore.sql.util;
 
