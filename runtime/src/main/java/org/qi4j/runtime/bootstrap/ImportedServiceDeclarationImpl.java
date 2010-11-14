3840c0f17137b564fa0f0ad76685590a4e961a33
Rickard Öberg <rickardoberg@gmail.com>
2010-11-14 16:54:42 +0100

    Imported services do not extend ServiceComposite yet. Duh.

--- runtime/src/main/java/org/qi4j/runtime/bootstrap/ImportedServiceDeclarationImpl.java
+++ runtime/src/main/java/org/qi4j/runtime/bootstrap/ImportedServiceDeclarationImpl.java
@@ -14,16 +14,16 @@
 
 package org.qi4j.runtime.bootstrap;
 
-import java.io.Serializable;
-import java.util.List;
 import org.qi4j.api.common.InvalidApplicationException;
 import org.qi4j.api.common.MetaInfo;
 import org.qi4j.api.common.Visibility;
-import org.qi4j.api.service.ServiceComposite;
 import org.qi4j.api.service.ServiceImporter;
 import org.qi4j.bootstrap.ImportedServiceDeclaration;
 import org.qi4j.runtime.service.ImportedServiceModel;
 import org.qi4j.spi.service.importer.InstanceImporter;
+
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * Declaration of an imported Service. Created by {@link ModuleAssemblyImpl#importServices(Class[])}.
