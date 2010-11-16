ff48252e5275c629104b94a3580382af7b074b17
Rickard Öberg <rickardoberg@gmail.com>
2010-11-16 11:00:18 +0100

    Fix for ReentrantReadWriteLock bug

--- locking/src/main/java/org/qi4j/library/locking/WriteLockConcern.java
+++ locking/src/main/java/org/qi4j/library/locking/WriteLockConcern.java
@@ -2,6 +2,7 @@ package org.qi4j.library.locking;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReadWriteLock;
 import org.qi4j.api.common.AppliesTo;
 import org.qi4j.api.concern.ConcernOf;
@@ -22,7 +23,7 @@ public class WriteLockConcern
     public Object invoke( Object o, Method method, Object[] objects )
         throws Throwable
     {
-        lock.writeLock().lock();
+        lock();
         try
         {
             return next.invoke( o, method, objects );
@@ -31,5 +32,24 @@ public class WriteLockConcern
         {
             lock.writeLock().unlock();
         }
+    }
+
+    /**
+     * Fix for this bug:
+     * http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6822370
+     */
+    protected void lock()
+    {
+       while (true)
+       {
+          try
+          {
+             lock.writeLock().tryLock( 1000, TimeUnit.MILLISECONDS );
+             break;
+          } catch (InterruptedException e)
+          {
+             // Try again
+          }
+       }
     }
 }
