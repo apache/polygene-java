bcc8a0d20278e04eca26487933ddf9034760e54e
Niclas Hedhman <niclas@hedhman.org>
2010-12-08 14:57:48 +0800

    Moving the Exceptions to the top level package.

--- shiro/core/src/main/java/org/qi4j/library/shiro/CryptoException.java
+++ shiro/core/src/main/java/org/qi4j/library/shiro/CryptoException.java
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.qi4j.library.shiro.crypto;
+package org.qi4j.library.shiro;
 
 import org.apache.shiro.ShiroException;
 
