8dc265e9ddf6ad119ec1eb787e26bd28fd1c8380
Niclas Hedhman <niclas@hedhman.org>
2010-12-09 14:41:15 +0800

    Serializable was removed from Composite. QI-304

--- osgi/src/test/java/org/qi4j/library/osgi/OSGiServiceTest.java
+++ osgi/src/test/java/org/qi4j/library/osgi/OSGiServiceTest.java
@@ -10,7 +10,8 @@ import org.qi4j.bootstrap.ModuleAssembly;
 import org.qi4j.test.AbstractQi4jTest;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class OSGiServiceTest extends AbstractQi4jTest
 {
@@ -34,13 +35,13 @@ public class OSGiServiceTest extends AbstractQi4jTest
         MyService service = (MyService) ref.get();
         service.value().set( 15 );
         assertEquals( (Integer) 15, service.value().get() );
-        String[] expectedClasses = new String[] { "org.qi4j.library.osgi.OSGiServiceTest$MyService",
-                                                  "org.qi4j.library.osgi.OSGiEnabledService",
-                                                  "org.qi4j.api.service.Activatable",
-                                                  "org.qi4j.api.service.ServiceComposite",
-                                                  "org.qi4j.api.entity.Identity",
-                                                  "org.qi4j.api.composite.Composite",
-                                                  "java.io.Serializable" };
+        String[] expectedClasses = new String[]{ "org.qi4j.library.osgi.OSGiServiceTest$MyService",
+                                                 "org.qi4j.library.osgi.OSGiEnabledService",
+                                                 "org.qi4j.api.service.Activatable",
+                                                 "org.qi4j.api.service.ServiceComposite",
+                                                 "org.qi4j.api.entity.Identity",
+                                                 "org.qi4j.api.composite.Composite"
+        };
         verify( bundleContext ).registerService( expectedClasses, service, null );
 
     }
