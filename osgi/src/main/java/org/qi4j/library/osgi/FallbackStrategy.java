bc737c9754427153ff535b94de5170f92c830ba5
Niclas Hedman <niclas.hedman@morganstanley.com>
2010-12-30 13:25:52 +0800

    Adding OSGi ServiceImporter.

---
+++ osgi/src/main/java/org/qi4j/library/osgi/FallbackStrategy.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2010 Morgan Stanley & Co. Incorporated, All Rights Reserved
+ *
+ * Unpublished copyright.  All rights reserved.  This material contains
+ * proprietary information that shall be used or copied only within
+ * Morgan Stanley, except with written permission of Morgan Stanley.
+ */
+package org.qi4j.library.osgi;
+
+
+import java.lang.reflect.Method;
+import org.osgi.framework.ServiceReference;
+
+/**
+ * The fallback strategy is invoked when the OSGi service is not available and a method call is invoked.
+ * <p>
+ * The FallbackStrategy is declared on the {@link OSGiServiceImporter} service declaration, like;
+ * <code><pre>
+ *     FallbackStrategy strategy = new MyStrategy();
+ *     module.addServices( OSGiServiceImporter.class )
+ *         .identifiedBy( "osgi" )
+ *         .setMetaInfo( bundleContext )
+ *         .setMetaInfo( strategy );
+ * </pre></code>
+ */
+public interface FallbackStrategy {
+    Object invoke(final ServiceReference reference, Method method, Object... args);
+}
