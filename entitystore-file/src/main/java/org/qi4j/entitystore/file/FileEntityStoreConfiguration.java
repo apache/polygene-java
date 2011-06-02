a18f9741d593bfa138c7f6c3375b1f36959cc540
Paul Merlin <paul@nosphere.org>
2011-06-02 15:36:30 +0200

    entitystore-file: some javadoc
    Conflicts:
    
    	.gitignore

--- entitystore-file/src/main/java/org/qi4j/entitystore/file/FileEntityStoreConfiguration.java
+++ entitystore-file/src/main/java/org/qi4j/entitystore/file/FileEntityStoreConfiguration.java
@@ -15,6 +15,8 @@ public interface FileEntityStoreConfiguration
      * The directory where the File Entity Store will be keep its persisted state.
      * <p>
      * Default: System.getProperty( "user.dir" ) + "/qi4j/filestore";
+     * <br/>
+     * Ignored if the FileConfiguration service is found.
      * </p>
      * <p>
      * The content inside this directory should not be modified directly, and doing so may corrupt the data.
