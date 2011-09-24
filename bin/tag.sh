afa3c3e983d65b4fb41062801c4e1deba4663485
Niclas Hedhman <niclas@hedhman.org>
2011-09-24 11:33:54 +0800

    Useful script.

--- bin/tag.sh
+++ bin/tag.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 VERSION=$1
+MESSAGE=$2
 cd core
-git tag -s -m "Release Candidate 5" $VERSION
+git tag -s -m $MESSAGE $VERSION
 git push --tags
 cd ..
 cd libraries
-git tag -s -m "Release Candidate 5" $VERSION
+git tag -s -m $MESSAGE $VERSION
 git push --tags
 cd ..
 cd extensions
-git tag -s -m "Release Candidate 5" $VERSION
+git tag -s -m $MESSAGE $VERSION
 git push --tags
 cd ..
-git tag -s -m "Release Candidate 5" $VERSION
+git tag -s -m $MESSAGE $VERSION
 git push --tags
