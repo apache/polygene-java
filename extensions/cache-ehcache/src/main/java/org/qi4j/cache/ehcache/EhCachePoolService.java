703a10febc91c66ee0cf99c82c830014b005e77f
Paul Merlin <paul@nosphere.org>
2012-07-26 17:26:10 +0200

    QI-364 Removed Activatable from the EHCache Cache Extension

--- extensions/cache-ehcache/src/main/java/org/qi4j/cache/ehcache/EhCachePoolService.java
+++ extensions/cache-ehcache/src/main/java/org/qi4j/cache/ehcache/EhCachePoolService.java
@@ -1,11 +1,42 @@
 package org.qi4j.cache.ehcache;
 
+import org.qi4j.api.activation.ActivatorAdapter;
+import org.qi4j.api.activation.Activators;
 import org.qi4j.api.mixin.Mixins;
 import org.qi4j.api.service.ServiceComposite;
+import org.qi4j.api.service.ServiceReference;
 import org.qi4j.spi.cache.CachePool;
 
 @Mixins( EhCachePoolMixin.class )
+@Activators( EhCachePoolService.Activator.class )
 public interface EhCachePoolService
-    extends CachePool, ServiceComposite
+        extends CachePool, ServiceComposite
 {
+
+    void activateCache()
+            throws Exception;
+
+    void passivateCache()
+            throws Exception;
+
+    class Activator
+            extends ActivatorAdapter<ServiceReference<EhCachePoolService>>
+    {
+
+        @Override
+        public void afterActivation( ServiceReference<EhCachePoolService> activated )
+                throws Exception
+        {
+            activated.get().activateCache();
+        }
+
+        @Override
+        public void beforePassivation( ServiceReference<EhCachePoolService> passivating )
+                throws Exception
+        {
+            passivating.get().passivateCache();
+        }
+
+    }
+
 }
