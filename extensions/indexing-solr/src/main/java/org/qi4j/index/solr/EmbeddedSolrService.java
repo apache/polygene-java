2a8376f09fc604aff5beacdad78c44ae683154be
Paul Merlin <paul@nosphere.org>
2012-07-26 17:19:21 +0200

    QI-364 Removed Activatable from the Indexing Solr Extension

--- extensions/indexing-solr/src/main/java/org/qi4j/index/solr/EmbeddedSolrService.java
+++ extensions/indexing-solr/src/main/java/org/qi4j/index/solr/EmbeddedSolrService.java
@@ -1,30 +1,70 @@
+/*
+ * Copyright (c) 2011, Rickard Öberg. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package org.qi4j.index.solr;
 
+import java.io.File;
+import java.lang.reflect.Field;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.core.CoreContainer;
 import org.apache.solr.core.SolrConfig;
 import org.apache.solr.core.SolrCore;
+import org.qi4j.api.activation.ActivatorAdapter;
+import org.qi4j.api.activation.Activators;
 import org.qi4j.api.injection.scope.Service;
 import org.qi4j.api.injection.scope.Uses;
 import org.qi4j.api.mixin.Mixins;
-import org.qi4j.api.service.Activatable;
 import org.qi4j.api.service.ServiceComposite;
 import org.qi4j.api.service.ServiceDescriptor;
+import org.qi4j.api.service.ServiceReference;
 import org.qi4j.library.fileconfig.FileConfiguration;
 
-import java.io.File;
-import java.lang.reflect.Field;
-
-@Mixins(EmbeddedSolrService.EmbeddedSolrServiceMixin.class)
-public interface EmbeddedSolrService extends Activatable, ServiceComposite
+@Mixins( EmbeddedSolrService.Mixin.class )
+@Activators( EmbeddedSolrService.Activator.class )
+public interface EmbeddedSolrService extends ServiceComposite
 {
-   public SolrServer getSolrServer();
+   SolrServer getSolrServer();
 
-   public SolrCore getSolrCore();
+   SolrCore getSolrCore();
+   
+    void activateSolr()
+            throws Exception;
 
-   abstract class EmbeddedSolrServiceMixin
-         implements Activatable,EmbeddedSolrService
+    void passivateSolr()
+            throws Exception;
+    
+    class Activator extends ActivatorAdapter<ServiceReference<EmbeddedSolrService>>
+    {
+
+        @Override
+        public void afterActivation( ServiceReference<EmbeddedSolrService> activated )
+                throws Exception
+        {
+            activated.get().activateSolr();
+        }
+
+        @Override
+        public void beforePassivation( ServiceReference<EmbeddedSolrService> passivating )
+                throws Exception
+        {
+            passivating.get().passivateSolr();
+        }
+        
+    }
+
+   abstract class Mixin
+         implements EmbeddedSolrService
    {
       @Service
       FileConfiguration fileConfig;
@@ -36,7 +76,8 @@ public interface EmbeddedSolrService extends Activatable, ServiceComposite
 
       private SolrCore core;
 
-      public void activate() throws Exception
+      @Override
+      public void activateSolr() throws Exception
       {
          ClassLoader oldCl = Thread.currentThread().getContextClassLoader();
          Thread.currentThread().setContextClassLoader( getClass().getClassLoader() );
@@ -58,7 +99,8 @@ public interface EmbeddedSolrService extends Activatable, ServiceComposite
          }
       }
 
-      public void passivate() throws Exception
+      @Override
+      public void passivateSolr() throws Exception
       {
          core.closeSearcher();
          coreContainer.shutdown();
