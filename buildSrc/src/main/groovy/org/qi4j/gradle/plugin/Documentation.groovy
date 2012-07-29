9c3f2f29f26fcf90775745cf9b125c61ea149e6b
Niclas Hedhman <niclas@hedhman.org>
2012-07-29 18:01:16 +0800

    More changes to build system, to populate the right things into the dist.

--- buildSrc/src/main/groovy/org/qi4j/gradle/plugin/Documentation.groovy
+++ buildSrc/src/main/groovy/org/qi4j/gradle/plugin/Documentation.groovy
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions
  * and limitations under the License.
  */
-package org.qi4j.gradle.plugin.documentation;
+package org.qi4j.gradle.plugin;
 
 
 import org.gradle.api.DefaultTask
