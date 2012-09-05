f44d0ed763369cba8575acb4713a1a0de1d11365
Paul Merlin <paul@nosphere.org>
2012-09-05 11:41:34 +0200

    EventSourcing Library: fix EventRouterTest

--- libraries/eventsourcing/src/test/java/org/qi4j/library/eventsourcing/domain/source/helper/EventRouterTest.java
+++ libraries/eventsourcing/src/test/java/org/qi4j/library/eventsourcing/domain/source/helper/EventRouterTest.java
@@ -1,7 +1,25 @@
+/*
+ * Copyright 2009-2010 Rickard Öberg AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.qi4j.library.eventsourcing.domain.source.helper;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.qi4j.api.value.ValueBuilder;
@@ -13,13 +31,9 @@ import org.qi4j.io.Receiver;
 import org.qi4j.library.eventsourcing.domain.api.DomainEventValue;
 import org.qi4j.library.eventsourcing.domain.api.UnitOfWorkDomainEventsValue;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.qi4j.test.util.JSONAssert.jsonObjectsEquals;
 
-/**
- * TODO
- */
 public class EventRouterTest
 {
     private List<UnitOfWorkDomainEventsValue> list;
@@ -70,7 +84,7 @@ public class EventRouterTest
     }
 
     @Test
-    public void testRouter() throws IOException
+    public void testRouter() throws IOException, JSONException
     {
         final List<DomainEventValue> matched = new ArrayList<DomainEventValue>(  );
         EventRouter<IOException> router = new EventRouter<IOException>();
@@ -85,6 +99,10 @@ public class EventRouterTest
 
         Inputs.iterable( Events.events( list ) ).transferTo( router );
 
-        Assert.assertThat(matched.toString(), CoreMatchers.equalTo( "[{\"name\":\"Test1\",\"entityType\":\"Foo\",\"entityId\":\"123\",\"parameters\":\"{}\"}, {\"name\":\"Test2\",\"entityType\":\"Foo\",\"entityId\":\"123\",\"parameters\":\"{}\"}]" ));
+        assertEquals( 2, matched.size() );
+        jsonObjectsEquals( new JSONObject( matched.get( 0 ).toString() ),
+                                      new JSONObject( "{\"name\":\"Test1\",\"entityType\":\"Foo\",\"entityId\":\"123\",\"parameters\":\"{}\"}" ) );
+        jsonObjectsEquals( new JSONObject( matched.get( 1 ).toString() ),
+                                      new JSONObject( "{\"name\":\"Test2\",\"entityType\":\"Foo\",\"entityId\":\"123\",\"parameters\":\"{}\"}" ) );
     }
 }
