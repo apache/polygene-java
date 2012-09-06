d7f27e3a6a0f846b4714bf6a34e1b94b306ce889
Niclas Hedhman <niclas@hedhman.org>
2012-09-07 00:57:59 +0800

    Removed QRM entity store.

--- extensions/entitystore-qrm/src/test/resources/org/qi4j/entitystore/qrm/testDbData.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO SA.PERSON ( ID, FIRST_NAME, LAST_NAME )
-VALUES
-( '1', 'John', 'Smith' ),
-( '2', 'Jane', 'Smith' ) ;
-
-INSERT INTO SA.ACCOUNT ( ID, NAME, CONTACT_ID )
-VALUES
-( '1', 'Janes Account','2' ),
-( '2', 'Janes Account 2','2' ) ;
