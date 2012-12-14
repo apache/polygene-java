be0ec36af12bdd04a0896898b8502fa23c0d83d3
Paul Merlin <paul@nosphere.org>
2012-12-14 16:50:01 +0100

    Website (minor): some paragraph refactoring

--- manual/src/resources/js/progressive-enhancement.js
+++ manual/src/resources/js/progressive-enhancement.js
@@ -226,14 +226,18 @@ $( document ).ready( function($){
         $("div.logo").append('\
             <p style="margin-top:2em; text-align: center"> \
                 <select style="font-size: 0.5em">\
-                    <option value="2.0" selected="selected">2.0-DEV</option>\
-                    <option value="1.4">1.4-STABLE</option>\
+                    <option value="2.0-SNAPSHOT" selected="selected">2.0-SNAPSHOT</option>\
+                    <option value="1.4">1.4</option>\
                 </select>\
             </p>');
         $("div.logo select").change(function() {
             switch( $(this).val() ) {
+                case "2.0-SNAPSHOT":
+                    window.location = "http://qi4j.org/";
+                    break;
                 case "1.4":
-                    window.location = "http://www2.qi4j.org/";
+                    window.location = "http://qi4j.org/releases/1.4/";
+                    break;
             }
         });
     }
