fa1615ee1f9869f7621f2c2499f08898c20c92e0
Paul Merlin <paul@nosphere.org>
2014-02-05 04:34:22 +0100

    Tests Regression: add a placeholder class to fix build when no source

---
+++ tests/regression/src/main/java/org/qi4j/test/regression/Regressions.java
@@ -0,0 +1,11 @@
+package org.qi4j.test.regression;
+
+/**
+ * Placeholder class to prevent the build to fail when no source code.
+ */
+public final class Regressions
+{
+    private Regressions()
+    {
+    }
+}
