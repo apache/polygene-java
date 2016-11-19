a766fe1aef728e7ad94ba06ce33358d77e27e3ed
Paul Merlin <paulmerlin@apache.org>
2016-11-19 14:16:34 +0100

    build: move website & manual build logic to ManualPlugin in buildSrc

--- buildSrc/src/main/groovy/org/apache/zest/gradle/doc/DocumentationTask.groovy
+++ buildSrc/src/main/groovy/org/apache/zest/gradle/doc/DocumentationTask.groovy
@@ -20,9 +20,14 @@ package org.apache.zest.gradle.doc
 import groovy.io.FileType
 import groovy.transform.CompileStatic
 import groovy.transform.TypeCheckingMode
-import org.apache.zest.gradle.release.ReleaseSpecExtension;
+import java.security.MessageDigest
+import org.apache.zest.gradle.ZestExtension
+import org.apache.zest.gradle.release.ReleaseSpecExtension
+import org.apache.zest.gradle.tasks.ExecLogged
+import org.gradle.api.Action;
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.CopySpec
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
@@ -37,23 +42,21 @@ class DocumentationTask extends DefaultTask
 {
   @Input def String docName
   @Input def String docType
-  void setDocName( String docName ) { this.docName = docName }
-  void setDocType( String docType ) { this.docType = docType }
 
   @InputDirectory def File getCommonResourcesDir() { project.file( 'src/resources' ) }
   @InputDirectory def File getConfigDir() { project.file( 'src/conf' ) }
-  @InputDirectory def File getDocsDir() { project.file( 'src/docs') }
-  @InputDirectory def File getSrcMainDir() { project.file( 'src/main') }
-  @InputDirectory def File getXslDir() { project.file( 'src/xsl') }
+  @InputDirectory def File getDocsDir() { project.file( 'src/docs' ) }
+  @InputDirectory def File getSrcMainDir() { project.file( 'src/main' ) }
+  @InputDirectory def File getXslDir() { project.file( 'src/xsl' ) }
   @InputDirectory def File getBuildSrcDir() { project.rootProject.file( 'buildSrc/src' ) }
 
   @InputFiles def getSubProjectsDocsDirs() { project.rootProject.subprojects.collect { p -> p.file( 'src/docs' ) } }
   @InputFiles def getSubProjectsTestDirs() { project.rootProject.subprojects.collect { p -> p.file( 'src/test' ) } }
 
-  @OutputDirectory def File getOutputDir() { project.file( "${project.buildDir}/docs/${docName}/" ) }
+  @OutputDirectory def File getOutputDir() { project.file( "${ project.buildDir }/docs/${ docName }/" ) }
 
-  def File getTempAsciidocDir() { project.file( "${project.buildDir}/tmp-asciidoc" ) }
-  def File getTempDir() { project.file( "${project.buildDir}/tmp/docs/${docName}") }
+  @Internal def File getTempAsciidocDir() { project.file( "${ project.buildDir }/tmp-asciidoc" ) }
+  @Internal def File getTempDir() { project.file( "${ project.buildDir }/tmp/docs/${ docName }" ) }
 
   @TaskAction
   def void generate()
@@ -73,13 +76,14 @@ class DocumentationTask extends DefaultTask
 
   def void installAsciidocFilters()
   {
-    def digester = java.security.MessageDigest.getInstance( 'SHA' )
+    def digester = MessageDigest.getInstance( 'SHA' )
     def filtersDir = project.rootProject.file( 'buildSrc/src/asciidoc/filters' )
     def userHome = new File( System.getProperty( 'user.home' ) )
     def dotAsciidocFiltersDir = new File( userHome, '.asciidoc/filters' )
     def installSnippets = false
-    filtersDir.eachFileRecurse( groovy.io.FileType.FILES ) { originalFile ->
-      def targetFile = new File( dotAsciidocFiltersDir, (originalFile.toURI() as String) - (filtersDir.toURI() as String) )
+    filtersDir.eachFileRecurse( FileType.FILES ) { originalFile ->
+      def targetFile = new File( dotAsciidocFiltersDir,
+                                 ( originalFile.toURI() as String ) - ( filtersDir.toURI() as String ) )
       if( !targetFile.exists() )
       {
         installSnippets = true
@@ -102,16 +106,18 @@ class DocumentationTask extends DefaultTask
         spec.into dotAsciidocFiltersDir
       }
       dotAsciidocFiltersDir.eachFileRecurse( FileType.FILES ) { file ->
-        if( file.name.endsWith( '.py' ) ) {
-          chmod(file, '755')
+        if( file.name.endsWith( '.py' ) )
+        {
+          chmod( file, '755' )
         }
       }
       println "Zest Asciidoc Filters Installed!"
     }
   }
 
-  @CompileStatic(TypeCheckingMode.SKIP)
-  def void chmod(File file, String permissions) {
+  @CompileStatic( TypeCheckingMode.SKIP )
+  def void chmod( File file, String permissions )
+  {
     ant.chmod( file: file.absolutePath, perm: permissions )
   }
 
@@ -128,30 +134,38 @@ class DocumentationTask extends DefaultTask
 
   def void generateAsciidocAccordingToReleaseSpecification()
   {
+    def zest = project.extensions.getByType( ZestExtension )
     project.copy { CopySpec spec ->
       spec.from docsDir
       spec.into tempAsciidocDir
       spec.include '**'
     }
-    if( project.version != '0' && !project.version.toString().contains( 'SNAPSHOT' ) ) {
+    if( zest.releaseVersion )
+    {
       def licenseFile = new File( tempAsciidocDir, 'userguide/libraries.txt' )
       def extensionsFile = new File( tempAsciidocDir, 'userguide/extensions.txt' )
       def toolsFile = new File( tempAsciidocDir, 'userguide/tools.txt' )
       [ licenseFile, extensionsFile, toolsFile ].each { asciidocFile ->
         def filteredFileContent = ''
         asciidocFile.readLines().each { line ->
-          if( line.startsWith( 'include::' ) ) {
+          if( line.startsWith( 'include::' ) )
+          {
             def approved = false
-            def releaseApprovedProjects = project.rootProject.extensions.getByType( ReleaseSpecExtension ).approvedProjects
-            releaseApprovedProjects.collect{it.projectDir}.each { approvedProjectDir ->
-              if( line.contains( "${approvedProjectDir.parentFile.name}/${approvedProjectDir.name}" ) ) {
+            def releaseApprovedProjects = project.rootProject.extensions.
+              getByType( ReleaseSpecExtension ).approvedProjects
+            releaseApprovedProjects.collect { it.projectDir }.each { approvedProjectDir ->
+              if( line.contains( "${ approvedProjectDir.parentFile.name }/${ approvedProjectDir.name }" ) )
+              {
                 approved = true
               }
             }
-            if( approved ) {
+            if( approved )
+            {
               filteredFileContent += "$line\n"
             }
-          } else {
+          }
+          else
+          {
             filteredFileContent += "$line\n"
           }
         }
@@ -162,32 +176,34 @@ class DocumentationTask extends DefaultTask
 
   def void generateXDoc()
   {
-    project.exec { ExecSpec spec ->
+    def outLog = getLogFile( 'adoc-2-docbook', 'stdout' )
+    def errLog = getLogFile( 'adoc-2-docbook', 'stderr' )
+    ExecLogged.execLogged( project, outLog, errLog, { ExecSpec spec ->
       spec.executable = 'asciidoc'
       spec.workingDir = '..'
       def commonResourcesPath = relativePath( project.rootDir, commonResourcesDir )
       def asciidocConfigPath = relativePath( project.rootDir, new File( configDir, 'asciidoc.conf' ) )
       def docbookConfigPath = relativePath( project.rootDir, new File( configDir, 'docbook45.conf' ) )
       def linkimagesConfigPath = relativePath( project.rootDir, new File( configDir, 'linkedimages.conf' ) )
-      def xdocOutputPath =  relativePath( project.rootDir, new File( tempDir, 'xdoc-temp.xml' ) )
+      def xdocOutputPath = relativePath( project.rootDir, new File( tempDir, 'xdoc-temp.xml' ) )
       def asciidocIndexPath = relativePath( project.rootDir, new File( tempAsciidocDir, "$docName/index.txt" ) )
       spec.args = [
-              '--attribute', 'revnumber=' + project.version,
-              '--attribute', 'level1=' + (docType.equals('article') ? 1 : 0),
-              '--attribute', 'level2=' + (docType.equals('article') ? 2 : 1),
-              '--attribute', 'level3=' + (docType.equals('article') ? 3 : 2),
-              '--attribute', 'level4=' + (docType.equals('article') ? 4 : 3),
-              '--attribute', 'importdir=' + commonResourcesPath,
-              '--backend', 'docbook',
-              '--attribute', 'docinfo1',
-              '--doctype', docType,
-              '--conf-file=' + asciidocConfigPath,
-              '--conf-file=' + docbookConfigPath,
-              '--conf-file=' + linkimagesConfigPath,
-              '--out-file', xdocOutputPath,
-              asciidocIndexPath
+        '--attribute', 'revnumber=' + project.version,
+        '--attribute', 'level1=' + ( docType == 'article' ? 1 : 0 ),
+        '--attribute', 'level2=' + ( docType == 'article' ? 2 : 1 ),
+        '--attribute', 'level3=' + ( docType == 'article' ? 3 : 2 ),
+        '--attribute', 'level4=' + ( docType == 'article' ? 4 : 3 ),
+        '--attribute', 'importdir=' + commonResourcesPath,
+        '--backend', 'docbook',
+        '--attribute', 'docinfo1',
+        '--doctype', docType,
+        '--conf-file=' + asciidocConfigPath,
+        '--conf-file=' + docbookConfigPath,
+        '--conf-file=' + linkimagesConfigPath,
+        '--out-file', xdocOutputPath,
+        asciidocIndexPath
       ]
-    }
+    } as Action<? super ExecSpec> )
   }
 
   def void generateChunkedHtml()
@@ -202,61 +218,74 @@ class DocumentationTask extends DefaultTask
       spec.into outputDir
       spec.include '**'
     }
-
-    project.exec { ExecSpec spec ->
+    def outLog = getLogFile( 'docbook-2-chunked-html', 'stdout' )
+    def errLog = getLogFile( 'docbook-2-chunked-html', 'stderr' )
+    ExecLogged.execLogged( project, outLog, errLog, { ExecSpec spec ->
       def xsltFile = "$docsDir/$docName/xsl/chunked.xsl"
       def outputPath = relativePath( project.projectDir, outputDir ) + '/'
       spec.executable = 'xsltproc'
       spec.args = [
-              '--nonet',
-              '--noout',
-              '--output', outputPath,
-              xsltFile,
-              "$tempDir/xdoc-temp.xml"
+        '--nonet',
+        '--noout',
+        '--output', outputPath,
+        xsltFile,
+        "$tempDir/xdoc-temp.xml"
       ]
-    }
+    } as Action<? super ExecSpec> )
   }
 
   def void generateSingleHtml()
   {
-    project.exec { ExecSpec spec ->
+    def outLog = getLogFile( 'docbook-2-html', 'stdout' )
+    def errLog = getLogFile( 'docbook-2-html', 'stderr' )
+    ExecLogged.execLogged( project, outLog, errLog, { ExecSpec spec ->
       // XML_CATALOG_FILES=
       String xsltFile = "$xslDir/xhtml.xsl"
       spec.executable = 'xsltproc'
       spec.args = [
-              '--nonet',
-              '--noout',
-              '--output', "$outputDir/${docName}.html",
-              xsltFile,
-              "$tempDir/xdoc-temp.xml"
+        '--nonet',
+        '--noout',
+        '--output', "$outputDir/${ docName }.html",
+        xsltFile,
+        "$tempDir/xdoc-temp.xml"
       ]
-    }
+    } as Action<? super ExecSpec> )
   }
 
   def void generatePdf()
   {
     // $ xsltproc --nonet ../docbook-xsl/fo.xsl article.xml > article.fo
-    // $ fop article.fo article.pdf
-    project.exec { ExecSpec spec ->
+    def outLog = getLogFile( 'docbook-2-fo', 'stdout' )
+    def errLog = getLogFile( 'docbook-2-fo', 'stderr' )
+    ExecLogged.execLogged( project, outLog, errLog, { ExecSpec spec ->
       String xsltFile = "$xslDir/fo.xsl"
       spec.executable = 'xsltproc'
       spec.args = [
         '--nonet',
-        '--output', "$tempDir/${docName}.fo",
+        '--output', "$tempDir/${ docName }.fo",
         xsltFile,
         "$tempDir/xdoc-temp.xml"
       ]
-    }
-    project.exec { ExecSpec spec ->
+    } as Action<? super ExecSpec> )
+
+    // $ fop article.fo article.pdf
+    outLog = getLogFile( 'fo-2-pdf', 'stdout' )
+    errLog = getLogFile( 'fo-2-pdf', 'stderr' )
+    ExecLogged.execLogged( project, outLog, errLog, { ExecSpec spec ->
       spec.executable = 'fop'
       spec.args = [
-        "$tempDir/${docName}.fo",
-        "$outputDir/${docName}.pdf"
+        "$tempDir/${ docName }.fo",
+        "$outputDir/${ docName }.pdf"
       ]
-    }
+    } as Action<? super ExecSpec> )
   }
 
-  def String relativePath( File root, File target )
+  private File getLogFile( String step, String stream )
+  {
+    return project.file( "${ project.buildDir }/tmp/${ name }/${ step }-${ stream }.log" )
+  }
+
+  private static String relativePath( File root, File target )
   {
     new File( root.toURI().relativize( target.toURI() ).toString() ).path
   }
