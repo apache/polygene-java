3d9d3795d09dd2cf5c2f47d896d081dd3ae2b927
Paul Merlin <paulmerlin@apache.org>
2017-05-15 11:26:44 +0200

    :extensions:entitystore-jdbm refine

--- extensions/entitystore-jdbm/src/main/java/org/apache/polygene/entitystore/jdbm/JdbmConfiguration.java
+++ extensions/entitystore-jdbm/src/main/java/org/apache/polygene/entitystore/jdbm/JdbmConfiguration.java
@@ -21,7 +21,6 @@ package org.apache.polygene.entitystore.jdbm;
 
 import org.apache.polygene.api.common.Optional;
 import org.apache.polygene.api.common.UseDefaults;
-import org.apache.polygene.api.configuration.ConfigurationComposite;
 import org.apache.polygene.api.property.Property;
 
 /**
@@ -29,7 +28,6 @@ import org.apache.polygene.api.property.Property;
  */
 // START SNIPPET: config
 public interface JdbmConfiguration
-        extends ConfigurationComposite
 {
     // END SNIPPET: config
    /**
