105d37b67fd0c1fea09068b0a5db4b49ac6d812f
niclas <niclas@hedhman.org>
2017-05-20 21:41:29 +0800

    Adding @HistPortList constraint as well.
    Signed-off-by: niclas <niclas@hedhman.org>

---
+++ libraries/constraints/src/test/java/org/apache/polygene/library/constraints/HostPortListConstraintTest.java
@@ -0,0 +1,127 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ */
+package org.apache.polygene.library.constraints;
+
+import org.apache.polygene.api.common.Optional;
+import org.apache.polygene.api.constraint.ConstraintViolationException;
+import org.apache.polygene.api.property.Property;
+import org.apache.polygene.bootstrap.AssemblyException;
+import org.apache.polygene.bootstrap.ModuleAssembly;
+import org.apache.polygene.library.constraints.annotation.HostPortList;
+import org.apache.polygene.test.AbstractPolygeneTest;
+import org.junit.Test;
+
+public class HostPortListConstraintTest extends AbstractPolygeneTest
+{
+
+    @Test
+    public void givenValidHostPortWhenSettingPropertyExpectSuccess()
+        throws Exception
+    {
+        SomeValue someValue = transientBuilderFactory.newTransient( SomeValue.class );
+        someValue.hostPort().set( "habba.zout.com:1234" );
+    }
+
+    @Test( expected = ConstraintViolationException.class )
+    public void givenInvalidHostNameWhenSettingPropertyExpectConstrainViolation()
+        throws Exception
+    {
+        SomeValue someValue = transientBuilderFactory.newTransient( SomeValue.class );
+        someValue.hostPort().set( "1:2:3_i:1234" );
+    }
+
+    @Test( expected = ConstraintViolationException.class )
+    public void givenInvalidPortNumberWhenSettingPropertyExpectConstrainViolation()
+        throws Exception
+    {
+        SomeValue someValue = transientBuilderFactory.newTransient( SomeValue.class );
+        someValue.hostPort().set( "1.2.3.4:123456" );
+    }
+
+    @Test
+    public void givenValidIp4NumberPortNumberWhenSettingPropertyExpectSuccess()
+        throws Exception
+    {
+        SomeValue someValue = transientBuilderFactory.newTransient( SomeValue.class );
+        someValue.hostPort().set( "1.2.3.4:1234" );
+    }
+
+    @Test
+    public void givenValidIp6NumberPortNumberWhenSettingPropertyExpectSuccess()
+        throws Exception
+    {
+        SomeValue someValue = transientBuilderFactory.newTransient( SomeValue.class );
+        someValue.hostPort().set( "[::1]:1234" );
+    }
+
+    @Test
+    public void givenValidListHostPortWhenSettingPropertyExpectSuccess()
+        throws Exception
+    {
+        SomeValue someValue = transientBuilderFactory.newTransient( SomeValue.class );
+        someValue.hostPort().set( "habba.zout.com:1234,12.34.56.78:1234" );
+    }
+
+    @Test( expected = ConstraintViolationException.class )
+    public void givenInvalidListHostNameWhenSettingPropertyExpectConstrainViolation()
+        throws Exception
+    {
+        SomeValue someValue = transientBuilderFactory.newTransient( SomeValue.class );
+        someValue.hostPort().set( "1.2.3.4:12,1:2:3_i:1234" );
+    }
+
+    @Test( expected = ConstraintViolationException.class )
+    public void givenInvalidListPortNumberWhenSettingPropertyExpectConstrainViolation()
+        throws Exception
+    {
+        SomeValue someValue = transientBuilderFactory.newTransient( SomeValue.class );
+        someValue.hostPort().set( "1.2.3.4:1234 1.2.3.4:123456" );
+    }
+
+    @Test
+    public void givenValidListIp4NumberPortNumberWhenSettingPropertyExpectSuccess()
+        throws Exception
+    {
+        SomeValue someValue = transientBuilderFactory.newTransient( SomeValue.class );
+        someValue.hostPort().set( "1.2.3.4:1234 google.com" );
+    }
+
+    @Test
+    public void givenValidListIp6NumberPortNumberWhenSettingPropertyExpectSuccess()
+        throws Exception
+    {
+        SomeValue someValue = transientBuilderFactory.newTransient( SomeValue.class );
+        someValue.hostPort().set( "[::1]:1234" );
+    }
+
+    @Override
+    public void assemble( ModuleAssembly module )
+        throws AssemblyException
+    {
+        module.transients( SomeValue.class );
+    }
+
+    public interface SomeValue
+    {
+        @HostPortList
+        @Optional
+        Property<String> hostPort();
+    }
+}
